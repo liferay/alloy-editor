9b0d2798e577f1e41b2f30225d3ca73794b9a6bc
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-06-13 20:13:07 +0200

    SelectionRegion will add directly its functions to editor's instance

--- src/selection-region.js
+++ src/selection-region.js
@@ -6,286 +6,303 @@
     CKEDITOR.SELECTION_TOP_TO_BOTTOM = 0;
     CKEDITOR.SELECTION_BOTTOM_TO_TOP = 1;
 
-    CKEDITOR.plugins.add(
-        'selectionregion',
-        {
-            createSelectionFromPoint: function(x, y) {
-                this.createSelectionFromRange(x, y, x, y);
-            },
+    function SelectionRegion() {}
 
-            createSelectionFromRange: function(startX, startY, endX, endY) {
-                var editor,
-                    end,
-                    endContainer,
-                    endOffset,
-                    range,
-                    start,
-                    startContainer,
-                    startOffset;
+    SelectionRegion.prototype = {
+        createSelectionFromPoint: function(x, y) {
+            this.createSelectionFromRange(x, y, x, y);
+        },
 
-                editor = this.editor;
+        createSelectionFromRange: function(startX, startY, endX, endY) {
+            var editor,
+                end,
+                endContainer,
+                endOffset,
+                range,
+                start,
+                startContainer,
+                startOffset;
 
-                if (typeof document.caretPositionFromPoint == 'function') {
-                    start = document.caretPositionFromPoint(startX, startY);
-                    end = document.caretPositionFromPoint(endX, endY);
+            editor = this.editor;
 
-                    startContainer = start.offsetNode;
-                    endContainer = end.offsetNode;
+            if (typeof document.caretPositionFromPoint == 'function') {
+                start = document.caretPositionFromPoint(startX, startY);
+                end = document.caretPositionFromPoint(endX, endY);
 
-                    startOffset = start.offset;
-                    endOffset = end.offset;
+                startContainer = start.offsetNode;
+                endContainer = end.offsetNode;
 
-                    range = this.createRange();
-                }
-                else if (typeof document.caretRangeFromPoint == 'function') {
-                    start = document.caretRangeFromPoint(startX, startY);
-                    end = document.caretRangeFromPoint(endX, endY);
+                startOffset = start.offset;
+                endOffset = end.offset;
 
-                    startContainer = start.startContainer;
-                    endContainer = end.startContainer;
+                range = this.createRange();
+            }
+            else if (typeof document.caretRangeFromPoint == 'function') {
+                start = document.caretRangeFromPoint(startX, startY);
+                end = document.caretRangeFromPoint(endX, endY);
 
-                    startOffset = start.startOffset;
-                    endOffset = end.startOffset;
+                startContainer = start.startContainer;
+                endContainer = end.startContainer;
 
-                    range = this.createRange();
-                }
+                startOffset = start.startOffset;
+                endOffset = end.startOffset;
 
-                if (range && document.getSelection) {
-                    range.setStart(new CKEDITOR.dom.node(startContainer), startOffset);
-                    range.setEnd(new CKEDITOR.dom.node(endContainer), endOffset);
+                range = this.createRange();
+            }
 
-                    this.getSelection().selectRanges([range]);
-                }
-                else if (typeof document.body.createTextRange == 'function') {
-                    var sel = this.getSelection();
+            if (range && document.getSelection) {
+                range.setStart(new CKEDITOR.dom.node(startContainer), startOffset);
+                range.setEnd(new CKEDITOR.dom.node(endContainer), endOffset);
 
-                    sel.unlock();
+                this.getSelection().selectRanges([range]);
+            }
+            else if (typeof document.body.createTextRange == 'function') {
+                var sel = this.getSelection();
 
-                    range = document.body.createTextRange();
-                    range.moveToPoint(startX, startY);
+                sel.unlock();
 
-                    var endRange = range.duplicate();
-                    endRange.moveToPoint(endX, endY);
+                range = document.body.createTextRange();
+                range.moveToPoint(startX, startY);
 
-                    range.setEndPoint('EndToEnd', endRange);
-                    range.select();
+                var endRange = range.duplicate();
+                endRange.moveToPoint(endX, endY);
 
-                    this.getSelection().lock();
-                }
-            },
+                range.setEndPoint('EndToEnd', endRange);
+                range.select();
 
-            getCaretRegion: function() {
-                var bookmarkNodeEl,
-                    bookmarks,
-                    docScrollX,
-                    docScrollXY,
-                    docScrollY,
-                    region,
-                    selection;
+                this.getSelection().lock();
+            }
+        },
 
+        getCaretRegion: function() {
+            var bookmarkNodeEl,
+                bookmarks,
+                docScrollX,
+                docScrollXY,
+                docScrollY,
+                region,
+                selection;
+
+            selection = this.getSelection();
+
+            bookmarks = selection.createBookmarks();
+            bookmarkNodeEl = bookmarks[0].startNode.$;
+
+            bookmarkNodeEl.style.display = 'inline-block';
+
+            region = new CKEDITOR.dom.element(bookmarkNodeEl).getClientRect();
+
+            bookmarkNodeEl.parentNode.removeChild(bookmarkNodeEl);
+
+            docScrollXY = this._getDocScrollXY();
+            docScrollX = docScrollXY[0];
+            docScrollY = docScrollXY[1];
+
+            return {
+                bottom: docScrollY + region.bottom,
+                left: docScrollX + region.left,
+                right: docScrollX + region.right,
+                top: docScrollY + region.top
+            };
+        },
+
+        getSelectionData: function() {
+            var result,
+                selection;
+
+            selection = this.getSelection();
+
+            result = {
+                element: selection.getSelectedElement(),
+                text: selection.getSelectedText()
+            };
+
+            result.region = this.getSelectionRegion(selection);
+
+            return result;
+        },
+
+        getSelectionRegion: function() {
+            var direction,
+                region;
+
+            direction = CKEDITOR.SELECTION_TOP_TO_BOTTOM;
+
+            region = this.getClientRectsRegion();
+
+            region.direction = this._getSelectionDirection();
+
+            region.height = region.bottom - region.top;
+            region.width = region.right - region.left;
+
+            return region;
+        },
+
+        isSelectionEmpty: function() {
+            var ranges,
                 selection = this.getSelection();
 
-                bookmarks = selection.createBookmarks();
-                bookmarkNodeEl = bookmarks[0].startNode.$;
+            return (selection.getType() === CKEDITOR.SELECTION_NONE) ||
+                ((ranges = selection.getRanges()) && ranges.length === 1 && ranges[0].collapsed);
+        },
 
-                bookmarkNodeEl.style.display = 'inline-block';
+        getClientRectsRegion: function() {
+            var bottom,
+                clientRects,
+                docScrollX,
+                docScrollXY,
+                docScrollY,
+                endRect,
+                i,
+                item,
+                left,
+                length,
+                nativeSelection,
+                range,
+                rangeCount,
+                region,
+                right,
+                selection,
+                startRect,
+                top;
 
-                region = new CKEDITOR.dom.element(bookmarkNodeEl).getClientRect();
+            selection = this.getSelection();
+            nativeSelection = selection.getNative();
 
-                bookmarkNodeEl.parentNode.removeChild(bookmarkNodeEl);
+            if (nativeSelection.createRange) {
+                range = nativeSelection.createRange();
+                clientRects = range.getClientRects();
+            }
+            else {
+                rangeCount = nativeSelection.rangeCount;
+                clientRects = nativeSelection.getRangeAt(0).getClientRects();
+            }
+
+            bottom = 0;
+            left = Infinity;
+            right = -Infinity;
+            top = Infinity;
+
+            if (clientRects.length === 0) {
+                region = this.getCaretRegion();
+            }
+            else {
+                for (i = 0, length = clientRects.length; i < length; i++) {
+                    item = clientRects[i];
+
+                    if (item.left < left) {
+                        left = item.left;
+                    }
+
+                    if (item.right > right) {
+                        right = item.right;
+                    }
+
+                    if (item.top < top) {
+                        top = item.top;
+                    }
+
+                    if (item.bottom > bottom) {
+                        bottom = item.bottom;
+                    }
+                }
 
                 docScrollXY = this._getDocScrollXY();
                 docScrollX = docScrollXY[0];
                 docScrollY = docScrollXY[1];
 
-                return {
-                    bottom: docScrollY + region.bottom,
-                    left: docScrollX + region.left,
-                    right: docScrollX + region.right,
-                    top: docScrollY + region.top
-                };
-            },
-
-            getSelectionData: function() {
-                var result,
-                    selection;
-
-                selection = this.getSelection();
-
-                result = {
-                    element: selection.getSelectedElement(),
-                    text: selection.getSelectedText()
+                region = {
+                    bottom: docScrollY + bottom,
+                    left: docScrollX + left,
+                    right: docScrollX + right,
+                    top: docScrollY + top
                 };
 
-                result.region = this.getSelectionRegion(selection);
+                if (clientRects.length) {
+                    endRect = clientRects[clientRects.length - 1];
+                    startRect = clientRects[0];
 
-                return result;
-            },
-
-            getSelectionRegion: function() {
-                var direction,
-                    region;
-
-                direction = CKEDITOR.SELECTION_TOP_TO_BOTTOM;
-
-                region = this.getClientRectsRegion();
-
-                region.direction = this._getSelectionDirection();
-
-                region.height = region.bottom - region.top;
-                region.width = region.right - region.left;
-
-                return region;
-            },
-
-            isSelectionEmpty: function() {
-                var ranges,
-                    selection = this.getSelection();
-
-                return (selection.getType() === CKEDITOR.SELECTION_NONE) ||
-                    ((ranges = selection.getRanges()) && ranges.length === 1 && ranges[0].collapsed);
-            },
-
-            getClientRectsRegion: function() {
-                var bottom,
-                    clientRects,
-                    docScrollX,
-                    docScrollXY,
-                    docScrollY,
-                    endRect,
-                    i,
-                    item,
-                    left,
-                    length,
-                    nativeSelection,
-                    range,
-                    rangeCount,
-                    region,
-                    right,
-                    selection,
-                    startRect,
-                    top;
-
-                selection = this.getSelection();
-                nativeSelection = selection.getNative();
-
-                if (nativeSelection.createRange) {
-                    range = nativeSelection.createRange();
-                    clientRects = range.getClientRects();
-                }
-                else {
-                    rangeCount = nativeSelection.rangeCount;
-                    clientRects = nativeSelection.getRangeAt(0).getClientRects();
-                }
-
-                bottom = 0;
-                left = Infinity;
-                right = -Infinity;
-                top = Infinity;
-
-                if (clientRects.length === 0) {
-                    region = this.getCaretRegion();
-                }
-                else {
-                    for (i = 0, length = clientRects.length; i < length; i++) {
-                        item = clientRects[i];
-
-                        if (item.left < left) {
-                            left = item.left;
-                        }
-
-                        if (item.right > right) {
-                            right = item.right;
-                        }
-
-                        if (item.top < top) {
-                            top = item.top;
-                        }
-
-                        if (item.bottom > bottom) {
-                            bottom = item.bottom;
-                        }
-                    }
-
-                    docScrollXY = this._getDocScrollXY();
-                    docScrollX = docScrollXY[0];
-                    docScrollY = docScrollXY[1];
-
-                    region = {
-                        bottom: docScrollY + bottom,
-                        left: docScrollX + left,
-                        right: docScrollX + right,
-                        top: docScrollY + top
+                    region.endRect = {
+                        bottom: docScrollY + endRect.bottom,
+                        height: endRect.height,
+                        left: docScrollX + endRect.left,
+                        right: docScrollX + endRect.right,
+                        top: docScrollY + endRect.top,
+                        width: endRect.width
                     };
 
-                    if (clientRects.length) {
-                        endRect = clientRects[clientRects.length - 1];
-                        startRect = clientRects[0];
+                    region.startRect = {
+                        bottom: docScrollY + startRect.bottom,
+                        height: startRect.height,
+                        left: docScrollX + startRect.left,
+                        right: docScrollX + startRect.right,
+                        top: docScrollY + startRect.top,
+                        width: startRect.width
+                    };
+                }
+            }
 
-                        region.endRect = {
-                            bottom: docScrollY + endRect.bottom,
-                            height: endRect.height,
-                            left: docScrollX + endRect.left,
-                            right: docScrollX + endRect.right,
-                            top: docScrollY + endRect.top,
-                            width: endRect.width
-                        };
+            return region;
+        },
 
-                        region.startRect = {
-                            bottom: docScrollY + startRect.bottom,
-                            height: startRect.height,
-                            left: docScrollX + startRect.left,
-                            right: docScrollX + startRect.right,
-                            top: docScrollY + startRect.top,
-                            width: startRect.width
-                        };
+        _getDocScrollXY: function() {
+            var docBody,
+                docDefaultView,
+                docElement,
+                pageXOffset,
+                pageYOffset;
+
+            docBody = document.body;
+            docDefaultView = document.defaultView;
+            docElement = document.documentElement;
+
+            pageXOffset = (docDefaultView) ? docDefaultView.pageYOffset : 0;
+            pageYOffset = (docDefaultView) ? docDefaultView.pageXOffset : 0;
+
+            return [
+                Math.max(docElement.scrollLeft, docBody.scrollLeft, pageXOffset),
+                Math.max(docElement.scrollTop, docBody.scrollTop, pageYOffset)
+            ];
+        },
+
+        _getSelectionDirection: function() {
+            var anchorNode,
+                direction,
+                nativeSelection,
+                position,
+                selection;
+
+            selection = this.getSelection();
+            nativeSelection = selection.getNative();
+
+            direction = CKEDITOR.SELECTION_TOP_TO_BOTTOM;
+
+            if ((anchorNode = nativeSelection.anchorNode) && anchorNode.compareDocumentPosition) {
+                position = anchorNode.compareDocumentPosition(nativeSelection.focusNode);
+
+                if (!position && nativeSelection.anchorOffset > nativeSelection.focusOffset || position === Node.DOCUMENT_POSITION_PRECEDING) {
+                    direction = CKEDITOR.SELECTION_BOTTOM_TO_TOP;
+                }
+            }
+
+            return direction;
+        }
+    };
+
+    CKEDITOR.plugins.add(
+        'selectionregion',
+        {
+            init: function(editor) {
+                var attr,
+                    hasOwnProperty;
+
+                hasOwnProperty = Object.prototype.hasOwnProperty;
+
+                for (attr in SelectionRegion.prototype) {
+                    if (hasOwnProperty.call(SelectionRegion.prototype, attr) && typeof editor[attr] == 'undefined') {
+                        editor[attr] = SelectionRegion.prototype[attr];
                     }
                 }
-
-                return region;
-            },
-
-            _getDocScrollXY: function() {
-                var docBody,
-                    docDefaultView,
-                    docElement,
-                    pageXOffset,
-                    pageYOffset;
-
-                docBody = document.body;
-                docDefaultView = document.defaultView;
-                docElement = document.documentElement;
-
-                pageXOffset = (docDefaultView) ? docDefaultView.pageYOffset : 0;
-                pageYOffset = (docDefaultView) ? docDefaultView.pageXOffset : 0;
-
-                return [
-                    Math.max(docElement.scrollLeft, docBody.scrollLeft, pageXOffset),
-                    Math.max(docElement.scrollTop, docBody.scrollTop, pageYOffset)
-                ];
-            },
-
-            _getSelectionDirection: function() {
-                var anchorNode,
-                    direction,
-                    nativeSelection,
-                    position,
-                    selection;
-
-                selection = this.getSelection();
-                nativeSelection = selection.getNative();
-
-                direction = CKEDITOR.SELECTION_TOP_TO_BOTTOM;
-
-                if ((anchorNode = nativeSelection.anchorNode) && anchorNode.compareDocumentPosition) {
-                    position = anchorNode.compareDocumentPosition(nativeSelection.focusNode);
-
-                    if (!position && nativeSelection.anchorOffset > nativeSelection.focusOffset || position === Node.DOCUMENT_POSITION_PRECEDING) {
-                        direction = CKEDITOR.SELECTION_BOTTOM_TO_TOP;
-                    }
-                }
-
-                return direction;
             }
         }
     );
