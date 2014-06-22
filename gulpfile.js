e3b262af69ebe9608cf268b0ba9a3a7f242e6888
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-06-22 09:15:40 +0200

    CSS split to core and skin and converted to SASS (not finished)

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
-var gulp = require('gulp');
-var wrap = require('gulp-wrap');
-var rename = require('gulp-rename');
-var path = require('path');
-var sass = require('gulp-sass');
+var compass = require('gulp-compass'),
+	gulp = require('gulp'),
+	path = require('path');
 
 gulp.task('copy-js', function() {
 	return gulp.src(path.resolve(__dirname + '/src/**/*.js'))
 		.pipe(gulp.dest(path.resolve(__dirname + '/dist')));
 });
 
-gulp.task('sass', function () {
-    gulp.src(path.resolve(__dirname + '/src/**/*.scss'))
-        .pipe(sass())
-        .pipe(gulp.dest(path.resolve(__dirname + '/dist')));
+gulp.task('copy-css', function() {
+	return gulp.src(path.resolve(__dirname + '/src/**/*.css'))
+		.pipe(gulp.dest(path.resolve(__dirname + '/dist')));
 });
 
-gulp.task('default', ['copy-js', 'sass']);
+gulp.task('copy-fonts', function() {
+	return gulp.src(path.resolve(__dirname + '/src/assets/font/fonts/**/*.*'))
+		.pipe(gulp.dest(path.resolve(__dirname + '/dist/assets/font/fonts')));
+});
+
+gulp.task('compass', function() {
+	return gulp.src(path.resolve(__dirname + '/src/**/*.scss'))
+		.pipe(compass({
+			project: path.resolve(__dirname + '/src/assets'),
+			css: 'css',
+			sass: 'sass'
+		}));
+	});
+
+gulp.task('default', ['copy-js', 'copy-css', 'copy-fonts', 'compass']);
