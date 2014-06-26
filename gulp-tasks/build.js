b589b9eb101cbdfeeac9b65bb2992ea6e7404d90
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-06-26 12:09:46 +0200

    Add release gulp task

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp'),
-	runSequence = require('run-sequence');
+    path = require('path'),
+    runSequence = require('run-sequence'),
+    zip = require('gulp-zip'),
+
+    ROOT = path.join(__dirname, '..');
 
 gulp.task('build', function() {
     runSequence('clean', 'compass',
         ['copy-js', 'copy-css', 'copy-fonts', 'export-env', 'copy-ckeditor'],
-        'join-css');
+        'join-css', 'join-plugins');
+});
+
+gulp.task('release', function() {
+    runSequence('clean', 'compass',
+        ['copy-js', 'copy-css', 'copy-fonts', 'export-env', 'copy-ckeditor'],
+        'join-css', 'join-plugins', function() {
+            return gulp.src(
+                    path.join(ROOT, 'dist', '/**/*')
+                )
+                .pipe(zip('lreditor.zip'))
+                .pipe(gulp.dest(path.join(ROOT, 'dist')));
+        });
 });
--- gulp-tasks/join.js
+++ gulp-tasks/join.js
@@ -20,3 +20,12 @@ gulp.task('join-css', function() {
         .pipe(concat('lao-editor.css'))
         .pipe(gulp.dest(path.join(ROOT, 'dist', 'assets')));
 });
+
+gulp.task('join-plugins', function() {
+  gulp.src([
+        path.join(ROOT, 'src', 'core', '/**/*.js'),
+        path.join(ROOT, 'src', 'plugins', '/**/*.js')
+    ])
+    .pipe(concat('plugins.js'))
+    .pipe(gulp.dest(path.join(ROOT, 'dist', 'plugins')));
+});
