c941ae0080a78a18579464684f8a19201136f509
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-19 10:40:03 +0200

    Document merge function

--- src/core/tools.js
+++ src/core/tools.js
@@ -1,6 +1,23 @@
-;(function() {
+(function() {
     'use strict';
 
+    /**
+     * CKEDITOR.tools class utility which adds additional methods to those of CKEditor.
+     *
+     * @class CKEDITOR.tools
+     */
+
+    /**
+     * Returns a new object containing all of the properties of all the supplied
+     * objects. The properties from later objects will overwrite those in earlier
+     * objects.
+     *
+     * Passing in a single object will create a shallow copy of it.
+     *
+     * @method merge
+     * @param {Object} objects* One or more objects to merge.
+     * @return {Object} A new merged object.
+     */
     CKEDITOR.tools.merge = CKEDITOR.tools.merge || function() {
         var i = 0,
             key,
@@ -16,7 +33,7 @@
                     result[key] = obj[key];
                 }
             }
-       }
+        }
 
         return result;
     };
