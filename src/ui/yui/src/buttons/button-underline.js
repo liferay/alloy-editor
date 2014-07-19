55d8ac7c8515af6d2805476f893211468c2ef577
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-19 22:35:03 +0200

    Document Button Underline class

--- src/ui/yui/src/buttons/button-underline.js
+++ src/ui/yui/src/buttons/button-underline.js
@@ -1,8 +1,15 @@
-YUI.add('button-u', function (Y) {
+YUI.add('button-u', function(Y) {
     'use strict';
 
     var Lang = Y.Lang;
 
+    /**
+     * Button Underline. A standard AlloyUI/YUI class which allows to apply underline style to
+     * editor selection. Extends Y.Plugin.Base and mixes Y.ButtonBase extension.
+     *
+     * @class Y.ButtonU
+     * @constructor
+     */
     var Underline = Y.Base.create('underline', Y.Plugin.Base, [Y.ButtonBase], {
         TPL_CONTENT: '<i class="alloy-editor-icon-underline"></i>'
     }, {
@@ -11,6 +18,14 @@ YUI.add('button-u', function (Y) {
         NS: 'underline',
 
         ATTRS: {
+
+            /**
+             * @description Specifies the style, which should be applied to the current selection in editor.
+             *
+             * @attribute element
+             * @default 'u'
+             * @type String
+             */
             element: {
                 validator: Lang.isString,
                 value: 'u'
@@ -20,6 +35,6 @@ YUI.add('button-u', function (Y) {
 
     Y.ButtonU = Underline;
 
-},'', {
+}, '', {
     requires: ['button-base']
 });
