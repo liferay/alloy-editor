6bec4cc60eb48ab4fcfd1e6c7261bf8b64d4c02a
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-19 09:25:58 +0200

    Document debounce method.

--- src/core/debounce.js
+++ src/core/debounce.js
@@ -1,6 +1,25 @@
-;(function() {
+(function() {
     'use strict';
 
+    /**
+     * CKEDITOR.tools class utility. Provides an additional debounce method to CKEDITOR.tools.
+     *
+     * @class CKEDITOR.tools
+     */
+
+    /**
+     * Debounce util function. If a function execution is expensive, it might be debounced. This means
+     * that it will be executed after some amount of time after its last call. For example, if we attach a
+     * a function on scroll event, it might be called hundreds times per second. In this case it may be
+     * debounced with, let's say 100ms. The real execution of this function will happen 100ms after last
+     * scroll event.
+     *
+     * @method debounce
+     * @param {Function} callback The callback which has to be called after given timeout.
+     * @param {Number} timeout Timeout in milliseconds after which the callback will be called.
+     * @param {Object} context The context in which the callback will be called. This argument is optional.
+     * @param {Array} args An array of arguments which the callback will receive.
+     */
     CKEDITOR.tools.debounce = CKEDITOR.tools.debounce || function(callback, timeout, context, args) {
         var callFn,
             debounceHandle;
