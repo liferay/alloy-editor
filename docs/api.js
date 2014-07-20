a464f431445bd22e7ec382eb3470edc74d2ac39d
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-20 21:48:28 +0200

    Update API (auto generated)

--- docs/api.js
+++ docs/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "AlloyEditor",
+        "ButtonBase",
         "CKEDITOR.Link",
         "CKEDITOR.plugins.dropimages",
         "CKEDITOR.plugins.placeholder",
