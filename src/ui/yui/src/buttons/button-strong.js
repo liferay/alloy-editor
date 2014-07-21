43a4790055c91ce780ee242d3fe6a077d36acc59
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-21 20:43:29 +0200

    Document Document ButtonStrong class

--- src/ui/yui/src/buttons/button-strong.js
+++ src/ui/yui/src/buttons/button-strong.js
@@ -1,8 +1,13 @@
-YUI.add('button-strong', function (Y) {
+YUI.add('button-strong', function(Y) {
     'use strict';
 
     var Lang = Y.Lang;
 
+    /**
+     * The ButtonStrong class provides functionality for applying "strong" (bold) style to a text selection.
+     *
+     * @class ButtonStrong
+     */
     var Strong = Y.Base.create('strong', Y.Plugin.Base, [Y.ButtonBase], {
         TPL_CONTENT: '<i class="alloy-editor-icon-bold"></i>'
     }, {
@@ -11,6 +16,13 @@ YUI.add('button-strong', function (Y) {
         NS: 'strong',
 
         ATTRS: {
+            /**
+             * Specifies the element (style) which this button handles.
+             *
+             * @attribute element
+             * @default 'strong'
+             * @type String
+             */
             element: {
                 validator: Lang.isString,
                 value: 'strong'
@@ -20,6 +32,6 @@ YUI.add('button-strong', function (Y) {
 
     Y.ButtonStrong = Strong;
 
-},'', {
+}, '', {
     requires: ['button-base']
 });
