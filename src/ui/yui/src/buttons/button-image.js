91a0a56478adfd4b647e5ff9c28cb9d4514210ec
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-21 19:59:08 +0200

    Document Button Image

--- src/ui/yui/src/buttons/button-image.js
+++ src/ui/yui/src/buttons/button-image.js
@@ -1,12 +1,30 @@
-YUI.add('button-image', function (Y) {
+YUI.add('button-image', function(Y) {
     'use strict';
 
     var Lang = Y.Lang;
 
+    /**
+     * The ButtonImage class provides functionality for changing the alignment of an image.
+     *
+     * @class Y.ButtonImage
+     */
+
+    /**
+     * Fired when an image file is added as an element in the editor.
+     *
+     * @event imageadd
+     * @param {CKEDITOR.dom.element} el The created img element in editor.
+     */
     var BtnImage = Y.Base.create('image', Y.Plugin.Base, [Y.ButtonBase], {
-    	_getInputFile: function() {
-            var instance = this,
-                id,
+        /**
+         * Creates an input element of type "file" if not already created and registers
+         * an event on input value change.
+         *
+         * @method _getInputFile
+         * @protected
+         */
+        _getInputFile: function() {
+            var id,
                 inputFile;
 
             inputFile = this._inputFile;
@@ -18,22 +36,7 @@ YUI.add('button-image', function (Y) {
 
                 inputFile = Y.one('#' + id);
 
-                inputFile.on('change', function() {
-                    var el,
-                        reader;
-
-                    reader = new FileReader();
-
-                    reader.onload = function (event) {
-                        el = CKEDITOR.dom.element.createFromHtml('<img src="' + event.target.result + '">');
-
-                        instance.get('host').get('editor').insertElement(el);
-                    };
-
-                    reader.readAsDataURL(inputFile.getDOMNode().files[0]);
-
-                    inputFile.set('value', '');
-                });
+                inputFile.on('change', this._onInputChange, this);
 
                 this._inputFile = inputFile;
             }
@@ -41,10 +44,51 @@ YUI.add('button-image', function (Y) {
             return inputFile;
         },
 
-    	_onClick: function() {
+        /**
+         * Simulates "click" event on the input field when user presses the button. This
+         * triggers the native open file dialog from the browser. This method works on IE9+.
+         *
+         * @method _onClick
+         * @protected
+         */
+        _onClick: function() {
             var inputFile = this._getInputFile();
 
             inputFile.simulate('click');
+        },
+
+        /**
+         * On input change, reads the chosen file and creates an img element with src as Data URI.
+         * Then, fires an {{#crossLink "Y.ButtonImage/imageadd:event"}}{{/crossLink}} via CKEditor
+         * message system.
+         *
+         * @method _onInputChange
+         * @protected
+         */
+        _onInputChange: function() {
+            var instance = this,
+                editor,
+                inputFile,
+                el,
+                reader;
+
+            reader = new FileReader();
+
+            reader.onload = function(event) {
+                editor = instance.get('host').get('editor');
+
+                el = CKEDITOR.dom.element.createFromHtml('<img src="' + event.target.result + '">');
+
+                editor.insertElement(el);
+
+                editor.fire('imageadd', el);
+            };
+
+            inputFile = this._inputFile;
+
+            reader.readAsDataURL(inputFile.getDOMNode().files[0]);
+
+            inputFile.set('value', '');
         },
 
         TPL_CONTENT: '<i class="alloy-editor-icon-image"></i>'
@@ -54,6 +98,13 @@ YUI.add('button-image', function (Y) {
         NS: 'image',
 
         ATTRS: {
+            /**
+             * Specifies that this button will be not toggleable.
+             *
+             * @attribute toggle
+             * @default false
+             * @type Boolean
+             */
             toggle: {
                 validator: Lang.isBoolean,
                 value: false,
@@ -64,6 +115,6 @@ YUI.add('button-image', function (Y) {
 
     Y.ButtonImage = BtnImage;
 
-},'', {
+}, '', {
     requires: ['button-base', 'node-event-simulate']
 });
