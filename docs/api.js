99571a3af405f20ed06e61cd4e1f601eb3f40607
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-21 20:21:52 +0200

    Add API (auto-generated)

--- docs/api.js
+++ docs/api.js
@@ -2,14 +2,17 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "AlloyEditor",
+        "ButtonA",
+        "ButtonBase",
+        "ButtonImage",
+        "ButtonLeft",
+        "ButtonU",
         "CKEDITOR.Link",
         "CKEDITOR.plugins.dropimages",
         "CKEDITOR.plugins.placeholder",
         "CKEDITOR.plugins.selectionregion",
         "CKEDITOR.plugins.uicore",
-        "CKEDITOR.tools",
-        "Y.ButtonBase",
-        "Y.ButtonU"
+        "CKEDITOR.tools"
     ],
     "modules": [],
     "allModules": []
