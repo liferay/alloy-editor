03ff6b6d5dc9a774a2497801928b19e17ecfdf53
Iliyan Peychev <iliyan.peychev@gmail.com>
2014-07-21 20:43:44 +0200

    Document Document ButtonTwitter class

--- src/ui/yui/src/buttons/button-twitter.js
+++ src/ui/yui/src/buttons/button-twitter.js
@@ -1,14 +1,33 @@
-YUI.add('button-twitter', function (Y) {
+YUI.add('button-twitter', function(Y) {
     'use strict';
 
     var Lang = Y.Lang;
 
+    /**
+     * The ButtonTwitter class provides functionality for sending tweets based on the selected text.
+     *
+     * @class ButtonTwitter
+     */
     var Tweet = Y.Base.create('tweet', Y.Plugin.Base, [Y.ButtonBase], {
+        /**
+         * Overwrites the default behaviour of ButtonBase's
+         * {{#crossLink "ButtonBase/updateUI:method"}}{{/crossLink}} method since
+         * this button does is not toggleable.
+         *
+         * @method updateUI
+         */
         updateUI: function() {
             // NOP
         },
 
-    	_onClick: function() {
+        /**
+         * Reads the selected text from the editor and opens a browser window so the
+         * user will be able to tweet directly the text.
+         *
+         * @method _onClick
+         * @protected
+         */
+        _onClick: function() {
             var editor;
 
             editor = this.get('host').get('editor');
@@ -27,22 +46,53 @@ YUI.add('button-twitter', function (Y) {
         NS: 'tweet',
 
         ATTRS: {
+            /**
+             * Specifies if the button is toggleable, or not. ButtonTwitter will be not toggleable by default.
+             *
+             * @attribute toggle
+             * @default false
+             * @type Boolean
+             * @writeOnce 'initOnly'
+             */
             toggle: {
                 validator: Lang.isBoolean,
                 value: false,
                 writeOnce: 'initOnly'
             },
 
+            /**
+             * Specifies the URL where user can tweet the selected text.
+             *
+             * @attribute tweetURL
+             * @default 'https://twitter.com/intent/tweet?text='
+             * @type String
+             */
             tweetURL: {
                 validator: Lang.isString,
                 value: 'https://twitter.com/intent/tweet?text='
             },
 
+            /**
+             * Specifies the properties of the browser window where the selected text
+             * will appear.
+             *
+             * @attribute windowProperties
+             * @default 'resizable,status,width=400,height=250'
+             * @type String
+             */
             windowProperties: {
                 validator: Lang.isString,
                 value: 'resizable,status,width=400,height=250'
             },
 
+            /**
+             * Specifies the title of the browser window where the selected text
+             * will appear.
+             *
+             * @attribute windowTitle
+             * @default ''
+             * @type String
+             */
             windowTitle: {
                 validator: Lang.isString,
                 value: ''
@@ -52,6 +102,6 @@ YUI.add('button-twitter', function (Y) {
 
     Y.ButtonTwitter = Tweet;
 
-},'', {
-    requires: ['button-base', 'node-event-simulate']
+}, '', {
+    requires: ['button-base']
 });
