1a5d4ffa90a41b9a4204759cc45d3451894d5157
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-07-22 11:11:56 +0200

    Document LinkTooltip plugin

--- src/ui/yui/src/plugins/linktooltip.js
+++ src/ui/yui/src/plugins/linktooltip.js
@@ -1,204 +1,328 @@
-;(function() {
+(function() {
     'use strict';
 
     if (CKEDITOR.plugins.get('linktooltip')) {
         return;
     }
 
-    YUI.add('linktooltip', function (Y) {
+    YUI.add('linktooltip', function(Y) {
         var Lang = Y.Lang,
 
-        LinkTooltip = Y.Base.create('linktooltip', Y.Widget, [Y.WidgetPosition, Y.WidgetPositionConstrain, Y.WidgetAutohide], {
-            initializer: function() {
-                this._eventHandles = [];
-            },
+            /**
+             * The LinkTooltip class provides functionality for showing tooltip over link elements in editor.
+             * The class will be also registered as CKEDITOR plugin - {{#crossLink "CKEDITOR.plugins.linktooltip"}}{{/crossLink}}
+             *
+             * @class LinkTooltip
+             */
+            LinkTooltip = Y.Base.create('linktooltip', Y.Widget, [Y.WidgetPosition, Y.WidgetPositionConstrain, Y.WidgetAutohide], {
+                /**
+                 * Initializer lifecycle implementation for the LinkTooltip class.
+                 *
+                 * @method initializer
+                 * @protected
+                 * @param config {Object} Configuration object literal for the editor
+                 */
+                initializer: function() {
+                    this._eventHandles = [];
+                },
 
-            destructor: function() {
-                (new Y.EventHandle(this._eventHandles)).detach();
-            },
+                /**
+                 * Destructor lifecycle implementation for the LinkTooltip class. Destroys the iattached event listeners.
+                 *
+                 * @method destructor
+                 * @protected
+                 */
+                destructor: function() {
+                    (new Y.EventHandle(this._eventHandles)).detach();
+                },
 
-            renderUI: function() {
-                var content;
+                /**
+                 * Renders the link tooltip node on contentBox.
+                 *
+                 * @method renderUI
+                 */
+                renderUI: function() {
+                    var content;
 
-                content = Y.Node.create(this.TPL_CONTENT);
+                    content = Y.Node.create(this.TPL_CONTENT);
 
-                this.get('contentBox').appendChild(content);
+                    this.get('contentBox').appendChild(content);
 
-                this._linkPreview = content.one('.link-preview');
-            },
+                    this._linkPreview = content.one('.link-preview');
+                },
 
-            bindUI: function() {
-                var editor;
+                /**
+                 * Attaches event listeners such as mouseenter on link elements. Links, which have
+                 * "data-cke-default-link" attribute will be ignored. These links are considered as
+                 * internal; they are being created by automatically when user triggers link creation.
+                 * See {{#crossLink "ButtonA/_onClick:method"}}{{/crossLink}} for more information.
+                 *
+                 * @method bindUI
+                 * @protected
+                 */
+                bindUI: function() {
+                    var editor;
 
-                this._bindBBMouseEnter();
+                    this._bindBBMouseEnter();
 
-                editor = this.get('editor');
+                    editor = this.get('editor');
 
-                this._eventHandles.push(
-                    Y.one(editor.element.$).delegate('mouseenter', this._onLinkMouseEnter, 'a[href]:not([data-cke-default-link])', this, editor)
-                );
+                    this._eventHandles.push(
+                        Y.one(editor.element.$).delegate('mouseenter', this._onLinkMouseEnter, 'a[href]:not([data-cke-default-link])', this, editor)
+                    );
 
-                this.get('boundingBox').on('clickoutside', this._onClickOutside, this);
-            },
+                    this.get('boundingBox').on('clickoutside', this._onClickOutside, this);
+                },
 
-            _bindBBMouseLeave: function() {
-                var boundingBox;
+                /**
+                 * Adds mouseleave listener to the boundingBox.
+                 *
+                 * @method _bindBBMouseLeave
+                 * @protected
+                 */
+                _bindBBMouseLeave: function() {
+                    var boundingBox;
 
-                boundingBox = this.get('boundingBox');
+                    boundingBox = this.get('boundingBox');
 
-                this._bbMouseLeaveHandle = boundingBox.once('mouseleave', this._onBBMouseLeave, this);
-            },
+                    this._bbMouseLeaveHandle = boundingBox.once('mouseleave', this._onBBMouseLeave, this);
+                },
 
-            _bindBBMouseEnter: function() {
-                var boundingBox;
+                /**
+                 * Adds mouseenter listener to the boundingBox.
+                 *
+                 * @method _bindBBMouseEnter
+                 * @protected
+                 */
+                _bindBBMouseEnter: function() {
+                    var boundingBox;
 
-                boundingBox = this.get('boundingBox');
+                    boundingBox = this.get('boundingBox');
 
-                this._bbMouseEnterHandle = boundingBox.on('mouseenter', this._onBBMouseEnter, this);
-            },
+                    this._bbMouseEnterHandle = boundingBox.on('mouseenter', this._onBBMouseEnter, this);
+                },
 
-            _attachHiddenHandle: function() {
-                var instance = this;
+                /**
+                 * Hides the linktooltip container after some delay, specified by
+                 * {{#crossLink "LinkTooltip/hideDelay:attribute"}}{{/crossLink}} attribute.
+                 *
+                 * @method _attachHiddenHandle
+                 * @protected
+                 */
+                _attachHiddenHandle: function() {
+                    var instance = this;
 
-                this._hideHandle = setTimeout(
-                    function() {
-                        instance.hide();
-                    },
-                    this.get('hideDelay')
-                );
-            },
+                    this._hideHandle = setTimeout(
+                        function() {
+                            instance.hide();
+                        },
+                        this.get('hideDelay')
+                    );
+                },
 
-            _getXY: function(x, y, link) {
-                var gutter,
-                    i,
-                    line,
-                    lineHeight,
-                    lines,
-                    region;
+                /**
+                 * Calculates and returns the most appropriate position where link tooltip should appear.
+                 * The position depends on the specified {{#crossLink "LinkTooltip/gutter:attribute"}}{{/crossLink}},
+                 * and on the fact if the link occupies more than one line in the editor.
+                 *
+                 * @method _getXY
+                 * @protected
+                 * returns {Array} An Array with the most appropriate x and y points in page coordinates.
+                 */
+                _getXY: function(x, y, link) {
+                    var gutter,
+                        i,
+                        line,
+                        lineHeight,
+                        lines,
+                        region;
 
-                lineHeight = parseInt(link.getComputedStyle('lineHeight'), 10);
+                    lineHeight = parseInt(link.getComputedStyle('lineHeight'), 10);
 
-                gutter = this.get('gutter');
+                    gutter = this.get('gutter');
 
-                region = link.get('region');
+                    region = link.get('region');
 
-                gutter = gutter.top;
+                    gutter = gutter.top;
 
-                if (Lang.isNumber(lineHeight)) {
-                    line = 1;
+                    if (Lang.isNumber(lineHeight)) {
+                        line = 1;
 
-                    lines =  Math.ceil((region.bottom - region.top) / lineHeight);
+                        lines = Math.ceil((region.bottom - region.top) / lineHeight);
 
-                    for (i = 1; i <= lines; i++) {
-                        if (y < region.top + lineHeight * i) {
-                            break;
+                        for (i = 1; i <= lines; i++) {
+                            if (y < region.top + lineHeight * i) {
+                                break;
+                            }
+
+                            ++line;
                         }
 
-                        ++line;
+                        y = region.top + line * lineHeight + gutter;
+                    } else {
+                        y = region.bottom + gutter;
                     }
 
-                    y = region.top + line * lineHeight + gutter;
-                }
-                else {
-                    y = region.bottom + gutter;
-                }
+                    return [x, y];
+                },
 
-                return [x, y];
-            },
+                /**
+                 * Handles mouse enter event on the boundingBox.
+                 * Clears any already started hide handle and attaches mouse leave listener to the boundingBox.
+                 *
+                 * @method _onBBMouseEnter
+                 * @protected
+                 */
+                _onBBMouseEnter: function() {
+                    clearTimeout(this._hideHandle);
 
-            _onBBMouseEnter: function() {
-                clearTimeout(this._hideHandle);
+                    this._bindBBMouseLeave();
+                },
 
-                this._bindBBMouseLeave();
-            },
+                /**
+                 * Handles mouse leave event on the boundingBox.
+                 * Clears any already started hide handle and attaches it again.
+                 *
+                 * @method _onBBMouseEnter
+                 * @protected
+                 */
+                _onBBMouseLeave: function() {
+                    clearTimeout(this._hideHandle);
 
-            _onBBMouseLeave: function() {
-                clearTimeout(this._hideHandle);
+                    this._attachHiddenHandle();
+                },
 
-                this._attachHiddenHandle();
-            },
+                /**
+                 * Hides the tooltip on clicking outside of the boundingBox.
+                 *
+                 * @method _onClickOutside
+                 * @protected
+                 */
+                _onClickOutside: function() {
+                    this.hide();
+                },
 
-            _onClickOutside: function() {
-                this.hide();
-            },
+                /**
+                 * Reads the link href, updates tooltip content with the read value,
+                 * shows the tooltip and attaches mouse leave listener on the boundingBox.
+                 *
+                 * @method _onLinkMouseEnter
+                 * @protected
+                 */
+                _onLinkMouseEnter: function(event) {
+                    var instance = this,
+                        link,
+                        linkText,
+                        xy;
 
-            _onLinkMouseEnter: function(event) {
-                var instance = this,
-                    link,
-                    linkText,
-                    xy;
+                    if (this._editMode) {
+                        return;
+                    }
 
-                if (this._editMode) {
-                    return;
-                }
-
-                clearTimeout(instance._hideHandle);
-
-                link = event.currentTarget;
-
-                linkText = link.getAttribute('href');
-
-                this._linkPreview.setAttribute('href', linkText);
-
-                this._linkPreview.set('innerHTML', Y.Escape.html(linkText));
-
-                instance.show();
-
-                xy = instance._getXY(event.pageX, event.pageY, link);
-
-                instance.set('xy', xy);
-
-                link.once('mouseleave', function() {
                     clearTimeout(instance._hideHandle);
 
-                    instance._attachHiddenHandle();
-                });
-            },
+                    link = event.currentTarget;
 
-            BOUNDING_TEMPLATE: '<div class="alloy-editor-tooltip-link"></div>',
+                    linkText = link.getAttribute('href');
 
-            TPL_CONTENT:
-                '<div class="link-container">' +
+                    this._linkPreview.setAttribute('href', linkText);
+
+                    this._linkPreview.set('innerHTML', Y.Escape.html(linkText));
+
+                    instance.show();
+
+                    xy = instance._getXY(event.pageX, event.pageY, link);
+
+                    instance.set('xy', xy);
+
+                    link.once('mouseleave', function() {
+                        clearTimeout(instance._hideHandle);
+
+                        instance._attachHiddenHandle();
+                    });
+                },
+
+                BOUNDING_TEMPLATE: '<div class="alloy-editor-tooltip-link"></div>',
+
+                TPL_CONTENT: '<div class="link-container">' +
                     '<span class="icon-link-container">' +
-                        '<i class="alloy-editor-icon-link"></i>' +
+                    '<i class="alloy-editor-icon-link"></i>' +
                     '</span>' +
                     '<a class="link-preview" target="_blank"></a>' +
-                '</div>'
-        }, {
-            ATTRS: {
-                constrain: {
-                    validator: Lang.isBoolean,
-                    value: true
-                },
+                    '</div>'
+            }, {
+                ATTRS: {
+                    /**
+                     * Specifies if the link tooltip show be constrained to the viewport.
+                     *
+                     * @attribute constrain
+                     * @default true
+                     * @type Boolean
+                     */
+                    constrain: {
+                        validator: Lang.isBoolean,
+                        value: true
+                    },
 
-                editor: {
-                    validator: Y.Lang.isObject
-                },
+                    /**
+                     * Contains the native editor implementation.
+                     *
+                     * @attribute editor
+                     * @default true
+                     * @type Object
+                     */
+                    editor: {
+                        validator: Y.Lang.isObject
+                    },
 
-                gutter: {
-                    validator: Lang.isObject,
-                    value: {
-                        top: 0,
-                        left: 0
+                    /**
+                     * Specifies the gutter of the toolbar. The gutter object contains the top and left
+                     * offsets from the point, where the tooltip is supposed to appear.
+                     *
+                     * @attribute gutter
+                     * @default {
+                     *   left: 0,
+                     *   top: 0
+                     * }
+                     * @type Object
+                     */
+                    gutter: {
+                        validator: Lang.isObject,
+                        value: {
+                            left: 0,
+                            top: 0
+                        }
+                    },
+
+                    /**
+                     * Specifies the delay after which the link tooltip will be hidden.
+                     *
+                     * @attribute hideDelay
+                     * @default 2000 (sec)
+                     * @type Number
+                     */
+                    hideDelay: {
+                        validator: Lang.isNumber,
+                        value: 2000
                     }
-                },
-
-                hideDelay: {
-                    validator: Lang.isNumber,
-                    value: 2000
                 }
-            }
-        });
+            });
 
         Y.LinkTooltip = LinkTooltip;
 
-    },'', {
+    }, '', {
         requires: ['dom-screen', 'escape', 'event-outside', 'node-event-delegate', 'event-mouseenter', 'widget-base', 'widget-position', 'widget-position-constrain', 'widget-autohide']
     });
 
+    /**
+     * CKEDITOR plugin which allows displaying tooltip over links in the editor.
+     * Internally it uses {{#crossLink "LinkTooltip"}}{{/crossLink}}. You may specify all
+     * properties which {{#crossLink "LinkTooltip"}}{{/crossLink}} accepts via editor.config.linktooltip property.
+     *
+     * @class CKEDITOR.plugins.linktooltip
+     */
     CKEDITOR.plugins.add(
-        'linktooltip',
-        {
+        'linktooltip', {
             init: function(editor) {
                 YUI().use('linktooltip', function(Y) {
                     var config,
