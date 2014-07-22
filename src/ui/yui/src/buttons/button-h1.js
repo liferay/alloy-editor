af3e9870fe88570d3214bba8b1e5b0d53a8e5a14
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-07-22 08:52:15 +0200

    Document ButtonH1

--- src/ui/yui/src/buttons/button-h1.js
+++ src/ui/yui/src/buttons/button-h1.js
@@ -1,8 +1,14 @@
-YUI.add('button-h1', function (Y) {
+YUI.add('button-h1', function(Y) {
     'use strict';
 
     var Lang = Y.Lang;
 
+    /**
+     * The ButtonH1 class provides functionality for applying HTML heading, level 1
+     * to the selection.
+     *
+     * @class Y.ButtonH1
+     */
     var H1 = Y.Base.create('h1', Y.Plugin.Base, [Y.ButtonBase], {
         TPL_CONTENT: '<i class="alloy-editor-icon-h1"></i>'
     }, {
@@ -11,6 +17,13 @@ YUI.add('button-h1', function (Y) {
         NS: 'h1',
 
         ATTRS: {
+            /**
+             * Specifies the element (style) which this button handles.
+             *
+             * @attribute element
+             * @default 'h1'
+             * @type String
+             */
             element: {
                 validator: Lang.isString,
                 value: 'h1'
@@ -20,6 +33,6 @@ YUI.add('button-h1', function (Y) {
 
     Y.ButtonH1 = H1;
 
-},'', {
+}, '', {
     requires: ['button-base']
 });
