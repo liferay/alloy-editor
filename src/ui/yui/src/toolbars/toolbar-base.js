c19efa68fc857cc0b4753b40e855a42c0f03c435
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-07-22 14:26:27 +0200

    Document ToolbarBase

--- src/ui/yui/src/toolbars/toolbar-base.js
+++ src/ui/yui/src/toolbars/toolbar-base.js
@@ -1,4 +1,4 @@
-YUI.add('toolbar-base', function (Y) {
+YUI.add('toolbar-base', function(Y) {
     'use strict';
 
     var Lang = Y.Lang,
@@ -6,7 +6,19 @@ YUI.add('toolbar-base', function (Y) {
 
     function ToolbarBase() {}
 
+    /**
+     * The ToolbarBase extension is a common base for all AlloyEditor's Toolbars.
+     *
+     * @class ToolbarBase
+     */
     ToolbarBase.prototype = {
+        /**
+         * Initializer lifecycle implementation for the ToolbarBase class.
+         *
+         * @method initializer
+         * @protected
+         * @param config {Object} Configuration object literal for the toolbar
+         */
         initializer: function() {
             var instance = this,
                 editor;
@@ -35,18 +47,49 @@ YUI.add('toolbar-base', function (Y) {
             editor.on('editorInteraction', instance._onEditorInteraction, instance);
         },
 
+        /**
+         * Fires {{#crossLink "ButtonBase/_afterActionPerformed:event"}}{{/crossLink}} so the buttons
+         * attached to Toolbar, which mixes this extension will be notified that user performed
+         * an action.
+         *
+         * @method _afterActionPerformed
+         * @protected
+         */
         _afterActionPerformed: function(event) {
             this.fire('actionPerformed', event);
         },
 
+        /**
+         * Returns the container in which all buttons are being rendered.
+         *
+         * @method _getButtonsContainer
+         * @protected
+         * @return {Node} The container of all buttons attached to the current isntance of Toolbar.
+         */
         _getButtonsContainer: function() {
             return this._buttonsContainer;
         },
 
+        /**
+         * Resolves and returns the name of the Button from the current editor configuration.
+         *
+         * @method _getButtonInstanceName
+         * @protected
+         * @return {String} The constructed name of Button.
+         * See {{#crossLink "CKEDITOR.plugins.uiloader/_getModules:method"}}{{/crossLink}}
+         * for more information about the rules for constructing button names.
+         */
         _getButtonInstanceName: function(buttonName) {
             return 'Button' + buttonName.substring(0, 1).toUpperCase() + buttonName.substring(1);
         },
 
+        /**
+         * Moves the Toolbar to a point in page coordinates. If transition was specified via the
+         * {{#crossLink "ToolbarBase/transition:attribute"}}{{/crossLink}}, it will be applied too.
+         *
+         * @method _moveToPoint
+         * @protected
+         */
         _moveToPoint: function(xy, direction) {
             var boundingBox,
                 boundingBoxNode,
@@ -67,8 +110,7 @@ YUI.add('toolbar-base', function (Y) {
 
                 if (direction === CKEDITOR.SELECTION_TOP_TO_BOTTOM) {
                     y = xy[1] - height;
-                }
-                else {
+                } else {
                     y = xy[1] + height;
                 }
 
@@ -80,8 +122,7 @@ YUI.add('toolbar-base', function (Y) {
                 boundingBox.setStyle('visibility', 'visible');
 
                 boundingBox.transition(transition);
-            }
-            else {
+            } else {
                 boundingBox.setStyle('visibility', 'visible');
 
                 this.set('xy', xy);
@@ -90,15 +131,39 @@ YUI.add('toolbar-base', function (Y) {
     };
 
     ToolbarBase.ATTRS = {
+        /**
+         * Returns the container in Toolbar's contentBox, where all buttons are rendered.
+         *
+         * @attribute buttonsContainer
+         * @type Node
+         * @readOnly
+         */
         buttonsContainer: {
             getter: '_getButtonsContainer',
             readOnly: true
         },
 
+        /**
+         * Contains the native editor implementation.
+         *
+         * @attribute editor
+         * @default true
+         * @type Object
+         */
         editor: {
             validator: Lang.isObject
         },
 
+        /**
+         * Specifies the transition which should be applied when Toolbars moves to given position.
+         *
+         * @attribute transition
+         * @default {
+         *    easing: 'ease-out',
+         *    duration: 0.1
+         *  }
+         * @type Object
+         */
         transition: {
             value: {
                 easing: 'ease-out',
@@ -109,6 +174,6 @@ YUI.add('toolbar-base', function (Y) {
 
     Y.ToolbarBase = ToolbarBase;
 
-},'', {
+}, '', {
     requires: ['plugin', 'node-base']
 });
