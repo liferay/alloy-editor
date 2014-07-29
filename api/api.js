cddcb58531da6842e9f0cd67d4cc544a992ea923
Chema Balsas <jose.balsas@liferay.com>
2014-07-29 09:25:38 +0200

    Add API docs

---
+++ api/api.js
@@ -0,0 +1,34 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AlloyEditor",
+        "ButtonA",
+        "ButtonBase",
+        "ButtonEm",
+        "ButtonH1",
+        "ButtonH2",
+        "ButtonImage",
+        "ButtonLeft",
+        "ButtonRight",
+        "ButtonStrong",
+        "ButtonTwitter",
+        "ButtonU",
+        "CKEDITOR.Link",
+        "CKEDITOR.plugins.dropimages",
+        "CKEDITOR.plugins.linktooltip",
+        "CKEDITOR.plugins.placeholder",
+        "CKEDITOR.plugins.selectionregion",
+        "CKEDITOR.plugins.uicore",
+        "CKEDITOR.plugins.uiloader",
+        "CKEDITOR.tools",
+        "LinkTooltip",
+        "ToolbarAdd",
+        "ToolbarAddItems",
+        "ToolbarBase",
+        "ToolbarImage",
+        "ToolbarStyles"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
