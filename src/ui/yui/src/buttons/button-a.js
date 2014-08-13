b0fab304b3fb34defd5ecfa46c3b9d92c2534e67
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-08-13 11:21:29 +0200

    Implement new skin
    Fixes #20

--- src/ui/yui/src/buttons/button-a.js
+++ src/ui/yui/src/buttons/button-a.js
@@ -464,7 +464,7 @@ YUI.add('button-a', function(Y) {
 
             TPL_LINK_CONTAINER: '<div class="link-wrapper hide">' +
                 '<div class="pull-left btn-group show-buttons-container">' +
-                '<button class="alloy-editor-button btn btn-default switch-to-edit"><i class="alloy-editor-icon-font"></i></button>' +
+                '<button class="alloy-editor-button btn btn-default switch-to-edit"><i class="alloy-editor-icon-edit"></i></button>' +
                 '</div>' +
                 '<div class="pull-left btn-group input-wrapper">' +
                 '<span class="input-container">' +
