6b8c7744f7c81d6ec31c113b15ecd3ea31fc3496
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2014-12-03 11:54:35 +0100

    AE-73 button quote

--- src/ui/yui/src/buttons/button-quote.js
+++ src/ui/yui/src/buttons/button-quote.js
@@ -4,15 +4,54 @@ YUI.add('button-quote', function(Y) {
     var Lang = Y.Lang;
 
     var BtnQuote = Y.Base.create('quote', Y.Plugin.Base, [Y.ButtonBase], {
-        _handleQuote: function(event) {
+        TPL_CONTENT: '<i class="alloy-editor-icon-quote-sign"></i>',
 
-        },
+        /**
+         * Applies the style of this button if its current status is pressed
+         * and removes it otherwise.
+         *
+         * @method _onClick
+         * @protected
+         * @param {EventFacade} event Event that triggered when user clicked on the button.
+         */
+        _onClick: function() {
+            var editor = this.get('host').get('editor');
 
-        TPL_CONTENT: '<i class="alloy-editor-icon-quote-sign"></i>'
+            editor.execCommand('blockquote');
+        }
     }, {
         NAME: 'quote',
 
-        NS: 'quote'
+        NS: 'quote',
+
+        ATTRS: {
+            /**
+             * Specifies the element (style) which this button handles.
+             *
+             * @attribute element
+             * @default 'blockquote'
+             * @type String
+             */
+            element: {
+                validator: Lang.isString,
+                value: 'blockquote'
+            },
+
+            /**
+             * Collection of strings used to label elements of the button's UI.
+             * ButtonQuote provides string properties to specify the label of the button.
+             *
+             * @attribute strings
+             * @default {label: 'Quotation'}
+             * @type Object
+             */
+            strings: {
+                validator: Lang.isObject,
+                value: {
+                    label: 'Quote'
+                }
+            }
+        }
     });
 
     Y.ButtonQuote = BtnQuote;
