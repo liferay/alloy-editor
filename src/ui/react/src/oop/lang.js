f38f8119ca4f78558f2acbfd09fd5d520b531288
Iliyan Peychev <iliyan.peychev@gmail.com>
2015-02-06 07:55:13 +0100

    Add more type checkers to Lang implementation

--- src/ui/react/src/oop/lang.js
+++ src/ui/react/src/oop/lang.js
@@ -6,12 +6,22 @@
             return Object.prototype.toString.call(param) == '[object Array]';
         },
 
+        isBoolean: function(param) {
+            return typeof param === 'boolean';
+        },
+
         isFunction: function(param) {
             return typeof(param) === 'function';
         },
 
         isNumber: function(param) {
             return typeof param === 'number' && isFinite(param);
+        },
+
+        isObject: function(param) {
+            var paramType = typeof param;
+
+            return (param && (paramType === 'object' || Lang.isFunction(param)));
         },
 
         isString: function(param) {
