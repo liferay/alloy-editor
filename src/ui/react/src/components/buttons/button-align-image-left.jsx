8d34ff8ade50cf16a56fa827c15120ec5e84c17d
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-02-28 20:58:40 -0300

    Document ButtonAlignImageLeft
    Fixes #107

--- src/ui/react/src/components/buttons/button-align-image-left.jsx
+++ src/ui/react/src/components/buttons/button-align-image-left.jsx
@@ -1,13 +1,25 @@
 (function () {
     'use strict';
 
+    /**
+     * The ButtonAlignImageLeft class provides functionality for aligning an image on left.
+     *
+     * @class ButtonAlignImageLeft
+     */
     var ButtonAlignImageLeft = React.createClass({
         mixins: [global.ButtonStyle, global.ButtonStateClasses, global.ButtonActionStyle],
 
+        /**
+         * Lifecycle. Provides static properties to the widget.
+         * - key: The name which will be used as an alias of the button in the configuration.
+         */
         statics: {
             key: 'imageLeft'
         },
 
+        /**
+         * Lifecycle. Returns the default values of the properties used in the widget.
+         */
         getDefaultProps: function() {
             return {
                 style: {
@@ -19,6 +31,11 @@
             };
         },
 
+        /**
+         * Lifecycle. Renders the UI of the button.
+         *
+         * @return {Object} The content which should be rendered.
+         */
         render: function() {
             return (
                 <button className="alloy-editor-button" data-type="button-align-left" onClick={this.handleClick}>
