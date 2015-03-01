83d66a0bbacb2e74a201959e3ed0641c809065fa
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-03-01 10:51:01 -0300

    Add skeleton test task for ReactUI
    Fixes #112

---
+++ src/ui/react/gulp-tasks/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+var karma = require('karma').server;
+var path = require('path');
+
+gulp.task('test', ['build'], function (done) {
+    // TODO: run karma here with the tests for ReactUI
+
+    done();
+});
