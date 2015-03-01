f43b91c762b6ed382ee5bac5cec4d50a65ff41b9
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-03-01 11:36:04 -0300

    Document button code
    Fixes #107

--- src/ui/react/src/components/buttons/button-code.jsx
+++ src/ui/react/src/components/buttons/button-code.jsx
@@ -1,13 +1,27 @@
 (function () {
     'use strict';
 
+    /**
+     * The ButtonCode class provides wraps a selection in `pre` element.
+     *
+     * @class ButtonCode
+     */
     var ButtonCode = React.createClass({
         mixins: [global.ButtonStyle, global.ButtonStateClasses, global.ButtonActionStyle],
 
+        /**
+         * Lifecycle. Provides static properties to the widget.
+         * - key: The name which will be used as an alias of the button in the configuration.
+         */
         statics: {
             key: 'code'
         },
 
+        /**
+         * Lifecycle. Returns the default values of the properties used in the widget.
+         *
+         * @return {Object} The default properties.
+         */
         getDefaultProps: function() {
             return {
                 style: {
@@ -16,6 +30,11 @@
             };
         },
 
+        /**
+         * Lifecycle. Renders the UI of the button.
+         *
+         * @return {Object} The content which should be rendered.
+         */
         render: function() {
             return (
                 <button className="alloy-editor-button" data-type="button-code" onClick={this.handleClick}>
