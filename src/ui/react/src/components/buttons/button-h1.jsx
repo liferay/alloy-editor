133a83c2c0784a26aa4b33c220bb2eaacf464639
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-03-01 11:36:15 -0300

    Document button h1
    Fixes #107

--- src/ui/react/src/components/buttons/button-h1.jsx
+++ src/ui/react/src/components/buttons/button-h1.jsx
@@ -1,13 +1,27 @@
 (function () {
     'use strict';
 
+    /**
+     * The ButtonH1 class provides wraps a selection in `h1` element.
+     *
+     * @class ButtonH1
+     */
     var ButtonH1 = React.createClass({
         mixins: [global.ButtonStyle, global.ButtonStateClasses, global.ButtonActionStyle],
 
+        /**
+         * Lifecycle. Provides static properties to the widget.
+         * - key: The name which will be used as an alias of the button in the configuration.
+         */
         statics: {
             key: 'h1'
         },
 
+        /**
+         * Lifecycle. Returns the default values of the properties used in the widget.
+         *
+         * @return {Object} The default properties.
+         */
         getDefaultProps: function() {
             return {
                 style: {
@@ -16,6 +30,11 @@
             };
         },
 
+        /**
+         * Lifecycle. Renders the UI of the button.
+         *
+         * @return {Object} The content which should be rendered.
+         */
         render: function() {
             return (
                 <button className="alloy-editor-button" data-type="button-h1" onClick={this.handleClick}>
