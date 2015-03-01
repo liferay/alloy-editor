1b2a1ccb02c19d8058fc8ce53aa4316f9925cac7
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-03-01 11:59:13 -0300

    Document button image
    Fixes #107

--- src/ui/react/src/components/buttons/button-image.jsx
+++ src/ui/react/src/components/buttons/button-image.jsx
@@ -1,13 +1,27 @@
 (function () {
     'use strict';
 
+    /**
+     * The ButtonImage class inserts an image to the content.
+     *
+     * @class ButtonImage
+     */
     var ButtonImage = React.createClass({
         mixins: [global.ButtonAction, global.ButtonStateClasses],
 
+        /**
+         * Lifecycle. Provides static properties to the widget.
+         * - key: The name which will be used as an alias of the button in the configuration.
+         */
         statics: {
             key: 'image'
         },
 
+        /**
+         * Lifecycle. Renders the UI of the button.
+         *
+         * @return {Object} The content which should be rendered.
+         */
         render: function() {
             return (
                 <button className="alloy-editor-button" data-type="button-image" onClick={this.handleClick}>
