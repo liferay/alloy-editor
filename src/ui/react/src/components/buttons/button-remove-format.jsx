bcfbc7421991601816f5d23b91f75145eafe6af7
Chema Balsas <jose.balsas@liferay.com>
2015-03-12 23:25:39 +0100

    Implements RemoveFormat button
    Fixes #121

---
+++ src/ui/react/src/components/buttons/button-remove-format.jsx
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    /**
+     * The ButtonHline class provides inserts horizontal line.
+     *
+     * @class ButtonHline
+     */
+    var ButtonRemoveFormat = React.createClass({
+        mixins: [global.ButtonCommand],
+
+        /**
+         * Lifecycle. Provides static properties to the widget.
+         * - key: The name which will be used as an alias of the button in the configuration.
+         */
+        statics: {
+            key: 'removeformat'
+        },
+
+        /**
+         * Lifecycle. Returns the default values of the properties used in the widget.
+         *
+         * @return {Object} The default properties.
+         */
+        getDefaultProps: function() {
+            return {
+                command: 'removeFormat'
+            };
+        },
+
+        /**
+         * Lifecycle. Renders the UI of the button.
+         *
+         * @return {Object} The content which should be rendered.
+         */
+        render: function() {
+            return (
+                <button className='alloy-editor-button' data-type="button-removeformat" onClick={this.handleClick} tabIndex={this.props.tabIndex}>
+                    <span className="alloy-editor-icon-removeformat"></span>
+                </button>
+            );
+        }
+    });
+
+    global.AlloyEditor.Buttons[ButtonRemoveFormat.key] = global.AlloyEditor.ButtonRemoveFormat = ButtonRemoveFormat;
+}());
