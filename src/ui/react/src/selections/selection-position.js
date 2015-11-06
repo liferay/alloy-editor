e1f25d41c6e9eb0d9357c07c099c63adea284589
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-11-06 21:44:00 +0100

    Source formatting
    Fixes #181

--- src/ui/react/src/selections/selection-position.js
+++ src/ui/react/src/selections/selection-position.js
@@ -1,44 +1,45 @@
 (function() {
     'use strict';
 
-    /**
-     * Default gutter value for toolbar positioning
-     * @type {Object}
-     */
+    // Default gutter value for toolbar positioning
     var DEFAULT_GUTTER = {
         left: 0,
         top: 0
     };
 
     /**
-     * [centerNode description]
-     * @param  {[type]} node [description]
-     * @param  {[type]} rect [description]
-     * @return {[type]}      [description]
+     * Centers a Toolbar according to given rectangle
+     *
+     * @method centerToolbar
+     * @param {Object} toolbar The toolbar to be centered
+     * @param {Object} rect The rectangle according to which the Toolbar will be centered
      */
     var centerToolbar = function(toolbar, rect) {
         var toolbarNode = ReactDOM.findDOMNode(toolbar);
 
         var halfNodeWidth = toolbarNode.offsetWidth / 2;
-        var scrollPos = new CKEDITOR.dom.window(window).getScrollPosition();
+        var scrollPosition = new CKEDITOR.dom.window(window).getScrollPosition();
 
         var gutter = toolbar.props.gutter || DEFAULT_GUTTER;
 
-        var widgetXY = toolbar.getWidgetXYPoint(rect.left + rect.width / 2 - scrollPos.x, rect.top + scrollPos.y, CKEDITOR.SELECTION_BOTTOM_TO_TOP);
+        var widgetXY = toolbar.getWidgetXYPoint(rect.left + rect.width / 2 - scrollPosition.x, rect.top + scrollPosition.y, CKEDITOR.SELECTION_BOTTOM_TO_TOP);
 
         toolbar.moveToPoint([
             widgetXY[0],
             widgetXY[1]
         ], [
-            rect.left + rect.width / 2 - halfNodeWidth - scrollPos.x,
-            rect.top - toolbarNode.offsetHeight + scrollPos.y - gutter.top
+            rect.left + rect.width / 2 - halfNodeWidth - scrollPosition.x,
+            rect.top - toolbarNode.offsetHeight + scrollPosition.y - gutter.top
         ]);
     };
 
     /**
-     * [imageSelectionSetPosition description]
-     * @param  {[type]} payload [description]
-     * @return {[type]}         [description]
+     * Sets the position of a toolbar according to the position of the selected image
+     *
+     * @method imageSelectionSetPosition
+     * @param {Object} payload Payload, should contain the selection data for retrieving the
+     * client rectangle of the selected image
+     * @return {Boolean} True, in all cases
      */
     var imageSelectionSetPosition = function(payload) {
         centerToolbar(this, payload.selectionData.element.getClientRect());
@@ -47,9 +48,12 @@
     };
 
     /**
-     * [tableSelectionSetPosition description]
-     * @param  {[type]} payload [description]
-     * @return {[type]}         [description]
+     * Sets the position of a toolbar according to the position of the selected image
+     *
+     * @method tableSelectionSetPosition
+     * @param {Object} payload Object, which contains the selection data for retrieving the
+     * client rectangle of the selected table
+     * @return {Boolean} True, in all cases
      */
     var tableSelectionSetPosition = function(payload) {
         var nativeEditor = payload.editor.get('nativeEditor');
