a59ffc78a3fbb6fba54f657709764372ef1ddcc5
Chema Balsas <jose.balsas@liferay.com>
2016-01-12 10:38:07 +0100

    Add selectionkeystrokes plugin
    Fixes #422

---
+++ src/plugins/selectionkeystrokes.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+
+    if (CKEDITOR.plugins.get('ae_selectionkeystrokes')) {
+        return;
+    }
+
+    /**
+     * CKEditor plugin that simulates editor interaction events based on manual keystrokes. This
+     * can be used to trigger different reactions in the editor.
+     *
+     * @class CKEDITOR.plugins.ae_selectionkeystrokes
+     */
+    CKEDITOR.plugins.add(
+        'ae_selectionkeystrokes', {
+            /**
+             * Initialization of the plugin, part of CKEditor plugin lifecycle.
+             * The function adds a command to the editor for every defined selectionKeystroke
+             * in the configuration and maps it to the specified keystroke.
+             *
+             * @method init
+             * @param {Object} editor The current editor instance
+             */
+            init: function(editor) {
+                if (editor.config.selectionKeystrokes) {
+                    var command, commandName;
+
+                    editor.config.selectionKeystrokes.forEach(function(selectionKeystroke) {
+                        command = new CKEDITOR.command(editor, {
+                            exec: function(editor) {
+                                editor.fire('editorInteraction', {
+                                    manualSelection: selectionKeystroke.selection,
+                                    nativeEvent: {},
+                                    selectionData: editor.getSelectionData()
+                                });
+                            }
+                        });
+
+                        commandName = 'selectionKeystroke' + selectionKeystroke.selection;
+
+                        editor.addCommand(commandName, command);
+                        editor.setKeystroke(selectionKeystroke.keys, commandName);
+                    });
+                }
+            }
+        }
+    );
+}());
