a825dd2504b5d0f0645809184ace2dcc3791af2f
antoniopol06 <antonio.pol@liferay.com>
2016-02-17 09:00:50 +0100

    Test button link edit with linkSchema to set default protocol
    Fixes #462

---
+++ src/ui/react/test/button-link-edit.jsx
@@ -0,0 +1,99 @@
+(function() {
+    'use strict';
+
+    var assert = chai.assert;
+    var Simulate = React.addons.TestUtils.Simulate;
+    var TestUtils = React.addons.TestUtils;
+
+    var KEY_L = 76;
+
+    describe('ButtonLink', function() {
+        this.timeout(35000);
+
+        before(Utils.createAlloyEditor);
+
+        after(Utils.destroyAlloyEditor);
+
+        beforeEach(Utils.beforeEach);
+
+        afterEach(Utils.afterEach);
+
+        it('should create link with default http protocol', function() {
+            bender.tools.selection.setWithHtml(this.nativeEditor, '{selection}');
+
+            var requestExclusiveListener = sinon.stub();
+
+            var cancelExclusive = sinon.stub();
+
+            var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} cancelExclusive={cancelExclusive} requestExclusive={requestExclusiveListener} />, this.container);
+
+            var inputLink = TestUtils.findRenderedDOMComponentWithTag(buttonLink, 'input');
+
+            TestUtils.Simulate.change(inputLink, { target: { value: 'link.com' } });
+
+            var buttonOk = TestUtils.findRenderedDOMComponentWithClass(buttonLink, 'ae-icon-ok');
+
+            Simulate.click(buttonOk.parentNode);
+
+            var data = bender.tools.getData(this.nativeEditor, {
+                fixHtml: true,
+                compatHtml: true
+            });
+
+            assert.strictEqual('<p><a href="http://link.com" target="">selection</a></p>', data);
+        });
+
+        it('should create link with specified http protocol', function() {
+            bender.tools.selection.setWithHtml(this.nativeEditor, '{selection}');
+
+            var requestExclusiveListener = sinon.stub();
+
+            var cancelExclusive = sinon.stub();
+
+            var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} cancelExclusive={cancelExclusive} requestExclusive={requestExclusiveListener} />, this.container);
+
+            var inputLink = TestUtils.findRenderedDOMComponentWithTag(buttonLink, 'input');
+
+            TestUtils.Simulate.change(inputLink, { target: { value: 'ftp://link.com' } });
+
+            var buttonOk = TestUtils.findRenderedDOMComponentWithClass(buttonLink, 'ae-icon-ok');
+
+            Simulate.click(buttonOk.parentNode);
+
+            var data = bender.tools.getData(this.nativeEditor, {
+                fixHtml: true,
+                compatHtml: true
+            });
+
+            assert.strictEqual('<p><a href="ftp://link.com" target="">selection</a></p>', data);
+        });
+
+        it('should create link with editor linkSchema configuration', function() {
+            bender.tools.selection.setWithHtml(this.nativeEditor, '{selection}');
+
+            this.nativeEditor.config.linkSchema = 'file://';
+
+            var requestExclusiveListener = sinon.stub();
+
+            var cancelExclusive = sinon.stub();
+
+            var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} cancelExclusive={cancelExclusive} requestExclusive={requestExclusiveListener} />, this.container);
+
+            var inputLink = TestUtils.findRenderedDOMComponentWithTag(buttonLink, 'input');
+
+            TestUtils.Simulate.change(inputLink, { target: { value: 'link.com' } });
+
+            var buttonOk = TestUtils.findRenderedDOMComponentWithClass(buttonLink, 'ae-icon-ok');
+
+            Simulate.click(buttonOk.parentNode);
+
+            var data = bender.tools.getData(this.nativeEditor, {
+                fixHtml: true,
+                compatHtml: true
+            });
+
+            assert.strictEqual('<p><a href="file://link.com" target="">selection</a></p>', data);
+        });
+
+    });
+}());
