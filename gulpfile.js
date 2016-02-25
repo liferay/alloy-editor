c3fc8884f76069975c24c7a779e8cdf17e3f7286
Chema Balsas <jose.balsas@liferay.com>
2016-02-25 12:28:36 +0100

    Use liferay-gulp-tasks to get maven-related tasks
    Fixes #473

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@
 
 var argv = require('yargs').argv,
 	gulp = require('gulp'),
+    liferayGulpTasks = require('liferay-gulp-tasks'),
 	path = require('path'),
     requireDir = require('require-dir'),
     ui = argv.ui || 'react';
+
+liferayGulpTasks.registerTasks();
 
 requireDir(path.join(__dirname, 'gulp-tasks'));
 requireDir(path.join(__dirname, 'src', 'ui', ui, 'gulp-tasks'));
