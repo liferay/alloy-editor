988c11ec21da315f8ab86713a461e10c0c41f126
Iliyan Peychev <iliyan.peychev@gmail.com>
2016-03-08 08:46:33 +0100

    Add a selection before firing paste event
    Firefox and IE10 require a selection
    before firing the `paste` event
    
    Fixes #478

--- src/ui/react/test/embed.js
+++ src/ui/react/test/embed.js
@@ -7,19 +7,48 @@
     describe('Embed', function() {
         this.timeout(35000);
 
-        beforeEach(Utils.createAlloyEditor);
+        var cleanUpEditor = function() {
+            if (this.alloyEditor) {
+                this.alloyEditor.destroy();
+                this.alloyEditor = null;
+            }
 
-        afterEach(Utils.destroyAlloyEditor);
+            this.el.parentNode.removeChild(this.el);
+        };
+
+        var initEditor = function(done, config) {
+            this.el = document.createElement('div');
+            this.el.setAttribute('id', 'editable');
+            document.body.appendChild(this.el);
+
+            this.alloyEditor = AlloyEditor.editable('editable', config);
+
+            this.alloyEditor.get('nativeEditor').once('instanceReady', function() {
+                done();
+            });
+        };
+
+        beforeEach(function(done) {
+            initEditor.call(this, done, {
+                allowedContent: true
+            });
+        });
+
+        afterEach(function() {
+            cleanUpEditor.call(this);
+        });
 
         it('should not convert links inside content', function() {
-        	this.nativeEditor.execCommand = sinon.stub();
-            this.nativeEditor.pasteFilter = null;
+            var nativeEditor = this.alloyEditor.get('nativeEditor');
+            bender.tools.selection.setWithHtml(nativeEditor, 'There should be a {selection} here.');
 
-            this.nativeEditor.fire('paste', {
+            nativeEditor.execCommand = sinon.stub();
+
+            nativeEditor.fire('paste', {
                 dataValue: 'this a <a href="http://test"></a> test'
             });
 
-            assert.isTrue(this.nativeEditor.execCommand.notCalled);
+            assert.isTrue(nativeEditor.execCommand.notCalled);
         });
     });
 }());
