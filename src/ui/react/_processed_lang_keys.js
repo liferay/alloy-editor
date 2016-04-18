c7f8828dc031f8b08de79319deb284cec8adb771
antoniopol06 <antonio.pol@liferay.com>
2016-04-18 09:00:49 +0200

    Add text translated for indent and outdent buttons
    Fixes #482

--- src/ui/react/_processed_lang_keys.js
+++ src/ui/react/_processed_lang_keys.js
@@ -21,6 +21,9 @@ AlloyEditor.Strings = {
 	h2: CKEDITOR.lang['{lang}'].format.tag_h2,
 	normal: CKEDITOR.lang['{lang}'].format.tag_p,
 
+	indent: CKEDITOR.lang['{lang}'].indent.indent,
+	outdent: CKEDITOR.lang['{lang}'].indent.outdent,
+
 	blockStyles: CKEDITOR.lang['{lang}'].stylescombo.panelTitle1,
 	inlineStyles: CKEDITOR.lang['{lang}'].stylescombo.panelTitle2,
 	objectStyles: CKEDITOR.lang['{lang}'].stylescombo.panelTitle3,
