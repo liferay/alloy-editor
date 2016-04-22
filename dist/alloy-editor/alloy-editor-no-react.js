4621cffca3c1a005d6505fcec0fa2d0bb7776c29
Iliyan Peychev <iliyan.peychev@gmail.com>
2016-04-22 15:22:36 +0200

    Add API and build files (auto-generated)
    Fixes #496

--- dist/alloy-editor/alloy-editor-no-react.js
+++ dist/alloy-editor/alloy-editor-no-react.js
@@ -13164,7 +13164,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
             return React.createElement(
                 'button',
                 { 'aria-label': AlloyEditor.Strings.deleteTable, className: 'ae-button', 'data-type': 'button-table-remove', onClick: this._removeTable, tabIndex: this.props.tabIndex, title: AlloyEditor.Strings.deleteTable },
-                React.createElement('span', { className: 'ae-icon-close' })
+                React.createElement('span', { className: 'ae-icon-bin' })
             );
         },
 
