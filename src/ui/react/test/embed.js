f3c5e573713a6ab2d81626fc91dae3457abe2a6d
antoniopol06 <antonio.pol@liferay.com>
2016-06-01 16:59:21 +0200

    Add tests to embed plugin Fixes #528

--- src/ui/react/test/embed.js
+++ src/ui/react/test/embed.js
@@ -40,6 +40,7 @@
 
         it('should not convert links inside content', function() {
             var nativeEditor = this.alloyEditor.get('nativeEditor');
+
             bender.tools.selection.setWithHtml(nativeEditor, 'There should be a {selection} here.');
 
             nativeEditor.execCommand = sinon.stub();
@@ -49,6 +50,77 @@
             });
 
             assert.isTrue(nativeEditor.execCommand.notCalled);
+        });
+
+        it('should create embed content when url is pasted', function(done) {
+            var url = 'https://foo.com';
+
+            var tweetReturnHtml = '<blockquote class="twitter-tweet" align="center">Hello Earth! Can you hear me?</blockquote>';
+
+            sinon.stub(CKEDITOR.tools, 'jsonp', function(fn, data, success, fail) {
+                success({html: tweetReturnHtml});
+            });
+
+            var nativeEditor = this.alloyEditor.get('nativeEditor');
+
+            bender.tools.selection.setWithHtml(nativeEditor, '{selection}');
+
+            nativeEditor.fire('paste', {
+                dataValue: url
+            });
+
+            setTimeout(function() {
+                assert.strictEqual(nativeEditor.getData(), '<div data-ae-embed-url="' + url + '">' + tweetReturnHtml + '</div>');
+                CKEDITOR.tools.jsonp.restore();
+                done();
+            }, 500);
+
+        });
+
+        it('should create embed content only with url when url is pasted and it does not retrieve content', function(done) {
+            var url = "http://bar.com";
+
+            sinon.stub(CKEDITOR.tools, 'jsonp', function(fn, data, success, fail) {
+                success({});
+            });
+
+            var nativeEditor = this.alloyEditor.get('nativeEditor');
+
+            bender.tools.selection.setWithHtml(nativeEditor, '{selection}');
+
+            nativeEditor.fire('paste', {
+                dataValue: url
+            });
+
+            setTimeout(function() {
+                assert.strictEqual(nativeEditor.getData(), '<div data-ae-embed-url="' + url + '">' + url + '</div>');
+                CKEDITOR.tools.jsonp.restore();
+                done();
+            }, 500);
+
+        });
+
+        it('should dont create embed content when wrong url (ie: without http protocol) is added', function(done) {
+            var url = "foo.com";
+
+            sinon.stub(CKEDITOR.tools, 'jsonp', function(fn, data, success, fail) {
+                fail({});
+            });
+
+            var nativeEditor = this.alloyEditor.get('nativeEditor');
+
+            bender.tools.selection.setWithHtml(nativeEditor, '{selection}');
+
+            nativeEditor.fire('paste', {
+                dataValue: url
+            });
+
+            setTimeout(function() {
+                assert.strictEqual('<p>' + url + '</p>', nativeEditor.getData());
+                CKEDITOR.tools.jsonp.restore();
+                done();
+            }, 500);
+
         });
     });
 }());
