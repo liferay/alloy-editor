dec20b82a58270b1532ba3d9c544a2532959a7f2
antoniopol06 <antonio.pol@liferay.com>
2016-09-22 18:03:22 +0200

    Test placeholder plugin
    This closes #589

--- test/plugins/test/placeholder.js
+++ test/plugins/test/placeholder.js
@@ -3,11 +3,11 @@
 
 	var assert = chai.assert;
 
-	describe.only('Placeholder', function() {
+	describe('Placeholder', function() {
 		this.timeout(35000);
 
 		before(function(done) {
-            Utils.createCKEditor.call(this, done, {extraPlugins: 'ae_placeholder'});
+            Utils.createCKEditor.call(this, done, {extraPlugins: 'ae_placeholder', placeholderClass: 'ae-placeholder'}, {'data-placeholder': 'This is placeholder'});
         });
 
         after(Utils.destroyCKEditor);
@@ -16,13 +16,17 @@
 
         afterEach(Utils.afterEach);
 
-        it('should placeholder appear when input is empty', function() {
-            var data = bender.tools.getData(this.nativeEditor, {
-                fixHtml: true,
-                compatHtml: true
-            });
+        it('should html value of content editor is the value when instance is created', function() {
+            assert.strictEqual(this.nativeEditor.editable().getHtml(), '<p><br></p>');
+            assert.isTrue(this.nativeEditor.element.hasClass('ae-placeholder'));
+        });
 
-            assert.strictEqual('<p>', data);
+        it('should html value of content editor is not changed after it is blur when its value is empty', function() {
+            this.nativeEditor.fire('blur');
+
+            assert.strictEqual(this.nativeEditor.editable().getHtml(), '<p><br></p>');
+            assert.isTrue(this.nativeEditor.element.hasClass('ae-placeholder'));
+
         });
 	});
 
