50361ee89bc895bdcedce35db45fb65b0dc7bf51
Iliyan Peychev <iliyan.peychev@gmail.com>
2016-12-03 09:47:45 +0100

    Source formatting
    Fixes #602

--- src/ui/react/test/plugins/test_buttonbridge/plugin.js
+++ src/ui/react/test/plugins/test_buttonbridge/plugin.js
@@ -1,33 +1,33 @@
 CKEDITOR.plugins.add('test_buttonbridge', {
-	init: function(editor) {
-		editor.addCommand('buttonCommand', {
-			exec: function() {
-				editor.fire('buttonCommand');
-			}
-		});
+    init: function (editor) {
+        editor.addCommand('buttonCommand', {
+            exec: function () {
+                editor.fire('buttonCommand');
+            }
+        });
 
-		if (editor.ui.addButton) {
-			editor.ui.addButton('ButtonCommand', {
-				command: 'buttonCommand'
-			});
+        if (editor.ui.addButton) {
+            editor.ui.addButton('ButtonCommand', {
+                command: 'buttonCommand'
+            });
 
-			editor.ui.addButton('ButtonClick', {
-				onClick: function() {
-					editor.fire('buttonClick');
-				}
-			});
+            editor.ui.addButton('ButtonClick', {
+                onClick: function () {
+                    editor.fire('buttonClick');
+                }
+            });
 
-			// Duplicated buttons should not override existing ones
-			editor.ui.addButton('ButtonClick', {
-				onClick: function() {
-					editor.fire('buttonClick2');
-				}
-			});
+            // Duplicated buttons should not override existing ones
+            editor.ui.addButton('ButtonClick', {
+                onClick: function () {
+                    editor.fire('buttonClick2');
+                }
+            });
 
-			editor.ui.addButton('PasteFromWord', {
-				command: 'pastefromword',
-				label: 'Paste From Word'
-			});
-		}
-	}
+            editor.ui.addButton('PasteFromWord', {
+                command: 'pastefromword',
+                label: 'Paste From Word'
+            });
+        }
+    }
 });
