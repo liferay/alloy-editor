c9524ae433790cc98623f478c929b8a46c510c92
Iliyan Peychev <iliyan.peychev@liferay.com>
2017-02-07 10:01:16 +0100

    Source formatting
    Fixes #688

--- src/ui/react/test/button-link-target-edit.jsx
+++ src/ui/react/test/button-link-target-edit.jsx
@@ -2,39 +2,37 @@
     'use strict';
 
     var assert = chai.assert;
-    var Simulate = React.addons.TestUtils.Simulate;
     var TestUtils = React.addons.TestUtils;
 
     describe('ButtonLinkTargetEdit', function() {
         this.timeout(35000);
 
-            before(Utils.createAlloyEditor);
+        before(Utils.createAlloyEditor);
 
-            after(Utils.destroyAlloyEditor);
+        after(Utils.destroyAlloyEditor);
 
-            beforeEach(Utils.beforeEach);
+        beforeEach(Utils.beforeEach);
 
-            afterEach(Utils.afterEach);
+        afterEach(Utils.afterEach);
 
-            it('should render ButtonLinkTargetEdit into ButtonLinkEdit by default', function() {
-                bender.tools.selection.setWithHtml(this.nativeEditor, 'There should be a <a>{selection}</a> made bold.');
+        it('should render ButtonLinkTargetEdit into ButtonLinkEdit by default', function() {
+            bender.tools.selection.setWithHtml(this.nativeEditor, 'There should be a <a>{selection}</a> made bold.');
 
-                var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} />, this.container);
+            var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} />, this.container);
 
-                var buttonLinkTargetEditRendered = TestUtils.scryRenderedComponentsWithType(buttonLink, AlloyEditor.ButtonLinkTargetEdit);
+            var buttonLinkTargetEditRendered = TestUtils.scryRenderedComponentsWithType(buttonLink, AlloyEditor.ButtonLinkTargetEdit);
 
-                assert.strictEqual(buttonLinkTargetEditRendered.length, 1);
-            });
+            assert.strictEqual(buttonLinkTargetEditRendered.length, 1);
+        });
 
-            it('should dont render ButtonLinkTargetEdit when showTargetSelector is false', function() {
-                bender.tools.selection.setWithHtml(this.nativeEditor, 'There should be a <a>{selection}</a> made bold.');
+        it('should dont render ButtonLinkTargetEdit when showTargetSelector is false', function() {
+            bender.tools.selection.setWithHtml(this.nativeEditor, 'There should be a <a>{selection}</a> made bold.');
 
-                var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} showTargetSelector={false}/>, this.container);
+            var buttonLink = ReactDOM.render(<AlloyEditor.ButtonLinkEdit editor={this.editor} showTargetSelector={false}/>, this.container);
 
-                var buttonLinkTargetEditRendered = TestUtils.scryRenderedComponentsWithType(buttonLink, AlloyEditor.ButtonLinkTargetEdit);
+            var buttonLinkTargetEditRendered = TestUtils.scryRenderedComponentsWithType(buttonLink, AlloyEditor.ButtonLinkTargetEdit);
 
-                assert.strictEqual(buttonLinkTargetEditRendered.length, 0);
-            });
-
+            assert.strictEqual(buttonLinkTargetEditRendered.length, 0);
+        });
     });
 }());
