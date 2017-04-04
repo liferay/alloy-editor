e6c4abfa40e5d19ad0a8e890850bac131816b43f
antoniopol06 <antonio.pol@liferay.com>
2017-04-04 15:50:04 +0200

    Merges configuration before replacing the dragresize plugins to make sure our choice prevails
    Fixes #716

--- src/ui/react/src/adapter/alloy-editor.js
+++ src/ui/react/src/adapter/alloy-editor.js
@@ -43,12 +43,12 @@
 
             editor.config.selectionKeystrokes = this.get('selectionKeystrokes');
 
+            AlloyEditor.Lang.mix(editor.config, config);
+
             if (CKEDITOR.env.ie) {
                 editor.config.extraPlugins = editor.config.extraPlugins.replace('ae_dragresize', 'ae_dragresize_ie');
                 editor.config.removePlugins = editor.config.removePlugins.replace('ae_dragresize', 'ae_dragresize_ie');
             }
-
-            AlloyEditor.Lang.mix(editor.config, config);
 
             editor.once('contentDom', function() {
 
