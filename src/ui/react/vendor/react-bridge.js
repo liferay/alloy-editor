4651faa79af4d31eac33bab8f915c22168446ac1
Bence Varga <vbence86@gmail.com>
2017-10-19 10:05:13 +0200

    removed unnecessary exposure to global scope

--- src/ui/react/vendor/react-bridge.js
+++ src/ui/react/vendor/react-bridge.js
@@ -4,9 +4,7 @@
 var PropTypes = (function() {
     return (0, eval)('this').PropTypes || React.PropTypes;
 }());
-(0, eval)('this').PropTypes = PropTypes;
 
 var createReactClass = (function() {
     return (0, eval)('this').createReactClass || React.createClass;
 }());    
-(0, eval)('this').createReactClass = createReactClass;
