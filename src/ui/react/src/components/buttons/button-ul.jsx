9987f85fbda2e1ee2a19b4d850308a08bc8a1567
Matthew Leffler <mattleff@gmail.com>
2018-02-19 16:45:38 -0700

    Use FA5 ol/ul icons

--- src/ui/react/src/components/buttons/button-ul.jsx
+++ src/ui/react/src/components/buttons/button-ul.jsx
@@ -76,7 +76,7 @@
 
             return (
                 <button aria-label={AlloyEditor.Strings.bulletedlist} aria-pressed={cssClass.indexOf('pressed') !== -1} className={cssClass} data-type="button-ul" onClick={this.execCommand} tabIndex={this.props.tabIndex} title={AlloyEditor.Strings.bulletedlist}>
-                    <span className="ae-icon-bulleted-list"></span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{fill:'currentColor'}} width="17" height="17"><path d="M506 114H134a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h372a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm6 154v-24a6 6 0 0 0-6-6H134a6 6 0 0 0-6 6v24a6 6 0 0 0 6 6h372a6 6 0 0 0 6-6zm0 160v-24a6 6 0 0 0-6-6H134a6 6 0 0 0-6 6v24a6 6 0 0 0 6 6h372a6 6 0 0 0 6-6zM48 60c-19.882 0-36 16.118-36 36s16.118 36 36 36 36-16.118 36-36-16.118-36-36-36zm0 160c-19.882 0-36 16.118-36 36s16.118 36 36 36 36-16.118 36-36-16.118-36-36-36zm0 160c-19.882 0-36 16.118-36 36s16.118 36 36 36 36-16.118 36-36-16.118-36-36-36z"></path></svg></span>
                 </button>
             );
         }
