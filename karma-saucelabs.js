61be070b691abe33446a116e89fcd1ea56e10708
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-04-04 12:35:52 -0300

    Use karma.common.js config

--- karma-saucelabs.js
+++ karma-saucelabs.js
@@ -1,132 +1,12 @@
-// Karma configuration
-
 'use strict';
 
-var alloyEditorDir = 'dist/alloy-editor/';
-var karmaSauceLauncher = require('karma-sauce-launcher');
+const Common = require('./karma.common');
+const karmaSauceLauncher = require('karma-sauce-launcher');
+const sauceLabsAccessKey = process.env.SAUCE_ACCESS_KEY_ENC;
 
-var argv = require('yargs').argv;
-var path = require('path');
-
-var srcFiles = require('./_src.js');
-srcFiles = srcFiles.main.concat(srcFiles.ui);
-
-var sauceLabsAccessKey = process.env.SAUCE_ACCESS_KEY_ENC;
 if (sauceLabsAccessKey) {
     sauceLabsAccessKey = new Buffer(sauceLabsAccessKey, 'base64').toString('binary');
 }
-
-var preprocessors = {
-    '**/*.jsx': ['babel'],
-    '**/*.html': ['html2js']
-};
-
-if (!(argv.debug || argv.d)) {
-    preprocessors[path.join(alloyEditorDir, 'test/**/*.js')] = ['coverage'];
-}
-
-var filesToLoad = [
-    /* AlloyEditor skins */
-    {
-        pattern: path.join(alloyEditorDir, 'assets/alloy-editor-ocean.css'),
-        included: true,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'assets/fonts/alloyeditor-ocean.woff'),
-        included: false,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'assets/fonts/alloyeditor-ocean.ttf'),
-        included: false,
-        watched: false
-    },
-
-    'test/vendor/zepto.js',
-    'test/vendor/happen.js',
-
-    /* CKEditor JS files */
-    {
-        pattern: path.join(alloyEditorDir, 'ckeditor.js'),
-        included: true,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'styles.js'),
-        included: true,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'config.js'),
-        included: true,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'skins/moono/*.css'),
-        included: true,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'lang/*.js'),
-        included: true,
-        watched: false
-    }, {
-        pattern: path.join(alloyEditorDir, 'plugins/test_*/plugin.js'),
-        included: true,
-        watched: false
-    },
-
-    /* bender requires CKEDITOR, should be after ckeditor.js */
-    'test/util/bender.js',
-
-    'test/util/utils.js',
-    'src/ui/react/test/util/utils.js',
-
-    /* ReactJS */
-    {
-        pattern: path.join(alloyEditorDir, 'react-with-addons-all.js'),
-        included: true,
-        watched: false
-    },
-
-    /* React Bridge */
-    {
-        pattern: path.join(alloyEditorDir, 'react-bridge.js'),
-        included: true,
-        watched: false
-    },
-
-    /* Fixtures */
-    'test/core/test/fixtures/**/*',
-    'src/ui/react/test/fixtures/**/*'
-];
-
-srcFiles.forEach(function(file) {
-    filesToLoad.push({
-        pattern: path.join(alloyEditorDir, 'test', file),
-        included: true,
-        watched: false
-    });
-});
-
-filesToLoad.push({
-    pattern: 'test/core/test/*.js*',
-    included: true,
-    watched: false
-});
-
-filesToLoad.push({
-    pattern: 'test/plugins/test/*.js*',
-    included: true,
-    watched: false
-});
-
-filesToLoad.push({
-    pattern: 'src/ui/react/test/*.js*',
-    included: true,
-    watched: false
-});
-
-filesToLoad.push({
-    pattern: path.join(alloyEditorDir, 'test/ui/react/lang/en.js'),
-    included: true,
-    watched: false
-});
 
 var customLaunchers = {
     sl_chrome: {
@@ -175,83 +55,49 @@ var customLaunchers = {
     }
 };
 
-var defaultConfig = {
+const defaultConfig = Object.assign(
+    Common,
+    {
+        browsers: Object.keys(customLaunchers),
 
-	browsers: Object.keys(customLaunchers),
+        browserDisconnectTimeout: 10000,
+        browserDisconnectTolerance: 2,
+        browserNoActivityTimeout: 240000,
 
-    browserDisconnectTimeout: 10000,
-    browserDisconnectTolerance: 2,
-    browserNoActivityTimeout: 240000,
+        captureTimeout: 240000,
+        customLaunchers: customLaunchers,
 
-    captureTimeout: 240000,
-    customLaunchers: customLaunchers,
+        plugins: [
+            'karma-babel-preprocessor',
+            'karma-coverage',
+            'karma-html2js-preprocessor',
+            'karma-chai',
+            'karma-fixture',
+            'karma-mocha',
+            'karma-sinon',
+            'karma-webpack',
+            karmaSauceLauncher
+        ],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../../..',
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'saucelabs'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['chai', 'fixture', 'mocha', 'sinon'],
-
-    // list of files / patterns to load in the browser
-    files: filesToLoad,
-
-    // list of files to exclude
-    exclude: [],
-
-    plugins: [
-        'karma-babel-preprocessor',
-        'karma-coverage',
-        'karma-html2js-preprocessor',
-        'karma-chai',
-        'karma-fixture',
-        'karma-mocha',
-        'karma-sinon',
-        karmaSauceLauncher
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: preprocessors,
-
-    babelPreprocessor: {
-      options: {
-        presets: ['es2015', 'react']
-      }
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'saucelabs'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: 'info',
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-    // soucelabs specific configuration
-    sauceLabs: {
-        accessKey: sauceLabsAccessKey,
-        testName: 'AlloyEditor tests',
-        recordVideo: false,
-        recordScreenshots: false,
-        startConnect: true,
-        connectOptions: {
-            port: 5757,
-            logfile: 'sauce_connect.log'
+        // soucelabs specific configuration
+        sauceLabs: {
+            accessKey: sauceLabsAccessKey,
+            testName: 'AlloyEditor tests',
+            recordVideo: false,
+            recordScreenshots: false,
+            startConnect: true,
+            connectOptions: {
+                port: 5757,
+                logfile: 'sauce_connect.log'
+            }
         }
     }
-
-};
+);
 
 module.exports = function(config) {
     config.set(defaultConfig);
