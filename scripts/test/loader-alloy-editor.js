98fba1965373617e954cfb8523c9e35a12c8f2ea
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-04-20 11:44:28 -0300

    Removes unnecessary exposure from OOP.extend, Attribute and Lang for the tests since we already have in the main

--- scripts/test/loader-alloy-editor.js
+++ scripts/test/loader-alloy-editor.js
@@ -1,9 +1,5 @@
 import * as AlloyEditor from '../../src/adapter/main';
 
-import extend from '../../src/oop/oop';
-import Attribute from '../../src/oop/attribute';
-import Lang from '../../src/oop/lang';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
@@ -24,14 +20,5 @@ import ReactTestUtils from 'react-dom/test-utils';
     if (!window.AlloyEditor) {
         window.AlloyEditor = AlloyEditor;
         window.ALLOYEDITOR_BASEPATH = 'dist/alloy-editor/';
-
-        window.AlloyEditor.Attribute = Attribute;
-        window.AlloyEditor.Lang = Lang;
-
-        if (!window.AlloyEditor.OOP) {
-            window.AlloyEditor.OOP = {
-                extend
-            };
-        }
     }
 }());
