0afdb5e6a046f518a5b110040cdc8bd60b565339
Julien Castelain <julien.castelain@liferay.com>
2018-11-15 15:21:52 +0100

    Update saucelabs configuration

--- src/ui/react/karma-saucelabs.js
+++ src/ui/react/karma-saucelabs.js
@@ -129,30 +129,39 @@ filesToLoad.push({
 });
 
 var customLaunchers = {
-    sl_chrome: {
+    sl_chrome_mac_10_11: {
         base: 'SauceLabs',
-        browserName: 'chrome'
+        browserName: 'chrome',
+        platform: 'OS X 10.11'
+    },
+    sl_chrome_mac_10_12: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'macOS 10.12'
+    },
+    sl_chrome_mac_10_13: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'macOS 10.13'
+    },
+    sl_chrome_linux: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Linux'
+    },
+    sl_chrome_windows: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 10'
     },
     sl_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox'
     },
-    sl_ie_9: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '9'
-    },
-    sl_ie_10: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '10'
-    },
     sl_ie_11: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 8.1',
+        platform: 'Windows 10',
         version: '11'
     },
     sl_edge_20: {
