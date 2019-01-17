77d873e48bc93511115819a174e72e738e6bb833
Julien Castelain <julien.castelain@liferay.com>
2019-01-17 17:10:56 +0100

    Sort imports

--- scripts/build/gulp/tasks/icons.js
+++ scripts/build/gulp/tasks/icons.js
@@ -1,8 +1,6 @@
-const gulp = require('gulp');
-
 const Constants = require('../constants');
+const gulp = require('gulp');
 const path = require('path');
-const fs = require('fs');
 
 gulp.task('copy-svgs', function() {
     var iconsPath = path.join(Constants.rootDir, 'node_modules', 'clay-css', 'lib', 'images', 'icons', 'icons.svg');
