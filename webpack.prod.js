6beba83eb0405e015e5626cfaa85bcdfbca58d28
Greg Hurrell <greg.hurrell@liferay.com>
2019-01-22 17:03:40 +0100

    Speed up minification by massaging settings
    Two things going on here:
    
    1. Turning off things with small gains based on this list:
    
        https://github.com/vuejs/vue-cli/blob/4ebf6700a403ca3c8e39387684acff6fca3632ac/packages/%40vue/cli-service/lib/config/terserOptions.js
    
    2. Switching from plug-in style to built-in `optimization` → `minimizer`
       style; surprisingly, this one seemed to have the biggest impact.
    
    With these, we go from 38s to 27s, and the build size doesn't change
    prohibitively:
    
      Before:
    
       874084 alloy-editor-all-min.js
       273571 alloy-editor-core-min.js
       383670 alloy-editor-no-ckeditor-min.js
       764321 alloy-editor-no-react-min.js
    
      After:
    
       881701 alloy-editor-all-min.js
       278273 alloy-editor-core-min.js
       389525 alloy-editor-no-ckeditor-min.js
       770769 alloy-editor-no-react-min.js
    
    Post-gzip, those size changes boil down to effectively nothing.

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,16 +5,41 @@ const TerserJsPlugin = require('terser-webpack-plugin');
 const config = {
 	mode: 'production',
 	devtool: 'source-map',
-	plugins: [
-		new TerserJsPlugin({
-			sourceMap: true,
-			terserOptions: {
-				output: {
-					comments: false
+
+	/**
+	 * https://webpack.js.org/configuration/optimization/
+	 */
+	optimization: {
+		minimizer: [
+			new TerserJsPlugin({
+				sourceMap: true,
+				terserOptions: {
+					compress: {
+						// Small gains, turned off to speed things up.
+						arrows: false,
+						collapse_vars: false,
+						comparisons: false,
+						computed_props: false,
+						hoist_funs: false,
+						hoist_props: false,
+						hoist_vars: false,
+						inline: false,
+						loops: false,
+						negate_iife: false,
+						properties: false,
+						reduce_funcs: false,
+						reduce_vars: false,
+						switches: false,
+						toplevel: false,
+						typeofs: false,
+					},
+					output: {
+						comments: false
+					}
 				}
-			}
-		})
-	]
+			})
+		]
+	}
 };
 
 const All = merge(common.config, config, {
