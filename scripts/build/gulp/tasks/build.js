59b023b3d4728a71198b91a9efa14e2c53cbbfae
Greg Hurrell <greg.hurrell@liferay.com>
2019-01-22 12:12:43 +0100

    Run webpack from inside gulp in parallel
    Before this commit, our build process looked like this:
    
    1. Run "build:assets" using the `gulp` executable.
    2. Do webpack.prod build using the `webpack` executable.
    3. Do webpack.dev build using the `webpack` executable.
    
    These serial processes took about 2s, 12s, and 38s respectively on my
    machine: a total of about 52s.
    
    After this commit, we do this:
    
    1. Use `gulp` to do everything the "build:assets" task did.
    2. In parallel, run the two webpack builds from the same `gulp`
       invocation.
    
    In the end, we wind shave only a few seconds from the original runtime
    (ie. going down to just under 50s) because we're always going to be
    limited by the length of the slowest job, and that job will actually run
    a little slower than usual when in parallel.
    
    I have some follow-ups I'll be doing after this, such as seeing if
    switching to a different minifier is faster (we're spending most of the
    time in Uglify). Another will be asking why we commit the "dist" files
    at all, which seems unconventional.
    
    Test plan:
    
    - Confirm in-browser that dev environment still works after doing `npm
      run dev` and `npm run start`.
    - Compare `dist` output with current HEAD of "develop" branch is
      identical.
    - Compare `dist` output is same for:
      - Gulp (dev): node_modules/.bin/gulp --gulpfile ...`
      - Webpack(dev): node_modules/.bin/webpack --config webpack.dev.js`
      - Gulp (prod): node_modules/.bin/gulp --gulpfile ... --release`
      - Webpack(prod): node_modules/.bin/webpack --config webpack.prod.js`

--- scripts/build/gulp/tasks/build.js
+++ scripts/build/gulp/tasks/build.js
@@ -6,19 +6,59 @@ const gulp = require('gulp');
 const path = require('path');
 const runSequence = require('run-sequence');
 const template = require('gulp-template');
+const webpack = require('webpack');
 const Constants = require('../constants');
 
-gulp.task('build', function(callback) {
-	const tasks = ['build-demo', 'post-cleanup'];
+runSequence.options.ignoreUndefinedTasks = true;
 
-	if (argv.release) {
-		tasks.push('minimize-css');
-	}
+function ifRelease(task) {
+	return !!argv.release && task;
+}
 
+function createBundle(configName, callback) {
+	const config = require(path.join(process.env.PWD, configName));
+	webpack(config, (error, {stats}) => {
+		if (error) {
+			callback(error);
+			return;
+		}
+		const errors = stats.reduce((acc, {compilation: {errors}}) => {
+			if (errors) {
+				acc.push(...errors);
+			}
+			return acc;
+		}, []);
+		const [firstError, ...remainingErrors] = errors;
+		if (firstError) {
+			remainingErrors.forEach(console.log.bind(console));
+			callback(firstError);
+		}
+		console.log(stats.toString({colors: true}));
+		callback();
+	});
+}
+
+gulp.task('webpack.prod', callback => {
+	createBundle('webpack.prod', callback);
+});
+
+gulp.task('webpack.dev', callback => {
+	createBundle('webpack.dev', callback);
+});
+
+gulp.task('build:assets', function(callback) {
 	runSequence(
 		'clean-dist',
 		['build-css', 'copy-ckeditor', 'copy-languages', 'copy-svgs'],
-		tasks,
+		['build-demo', 'post-cleanup', ifRelease('minimize-css')],
+		callback
+	);
+});
+
+gulp.task('build', function(callback) {
+	runSequence(
+		'build:assets',
+		[ifRelease('webpack.prod'), 'webpack.dev'],
 		callback
 	);
 });
