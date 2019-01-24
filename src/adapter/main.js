936929d7c0866b30490b7abe376db09e9a1b611f
Greg Hurrell <greg.hurrell@liferay.com>
2019-01-24 17:46:05 +0100

    Add explanation for why it is ok to suppress no-invalid-this lint
    This whole thing is fragile, but the truth is in the codebase we only
    ever call `AlloyEditor.[something}` so this is safe.

--- src/adapter/main.js
+++ src/adapter/main.js
@@ -198,7 +198,9 @@ const loadLanguageResources = function(callback) {
 					AlloyEditor.fire('languageResourcesLoaded');
 				}
 			},
-			this // eslint-disable-line no-invalid-this
+			/* eslint-disable no-invalid-this */
+			this // Always `AlloyEditor`.
+			/* eslint-enable no-invalid-this */
 		);
 	}
 };
