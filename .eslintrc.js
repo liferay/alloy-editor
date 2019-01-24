b8b391ede8245def05aacacbfb07745d531570dc
Greg Hurrell <greg.hurrell@liferay.com>
2019-01-24 12:19:11 +0100

    Add eslint-config-prettier
    This turns off any ESLint rules that might conflict with Prettier
    (things like indents, quotes etc).
    
    With this plugin configured, you can (in theory*) run things one after
    another without clobbering things:
    
    ```
    npm run fix # fixes fixable lints, but not formatting issues
    npm run format # fixes formatting issues
    npm run fix # won't make any changes
    ```
    
    (* Only in theory, because it looks like there are still a couple of
    extra rules that I'll need to manually tweak.)

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 			jsx: true,
 		},
 	},
-	extends: 'liferay',
+	extends: ['liferay', 'prettier'],
 	plugins: ['no-for-of-loops'],
 	rules: {
 		'no-for-of-loops/no-for-of-loops': 2,
