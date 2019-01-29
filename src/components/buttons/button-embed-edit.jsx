liferay/alloy-editor

commit 88ec95553cfc0590c4c5237e316fad5207585ca8

Fix for various buttons

src/components/buttons/button-embed-edit.jsx
@@ -138,11 +138,13 @@ class ButtonEmbedEdit extends React.Component {
 					/>
 					<button
 						aria-label={AlloyEditor.Strings.clearInput}
-						className="ae-button ae-icon-remove"
+						className="ae-button ae-button-clear"
 						onClick={this._clearLink}
 						style={clearLinkStyle}
-						title={AlloyEditor.Strings.clear}
-					/>
+						title={AlloyEditor.Strings.clear}>
+						<ButtonIcon editor={editor} symbol="times-clear" />
+					</button>
+					>
 				</div>
 				<button
 					aria-label={AlloyEditor.Strings.confirm}

src/components/base/widget-dropdown.js
@@ -50,13 +50,13 @@ export default WrappedComponent =>
 		 * @param {Object} itemKey They key of an React Widget which contains the dropdown.
 		 * @return {Object} The merged object.
 		 */
-		mergeDropdownProps(obj, itemKey) {
+		mergeDropdownProps = (obj, itemKey) => {
 			return CKEDITOR.tools.merge(obj, {
 				expanded: this.state.itemDropdown === itemKey ? true : false,
 				tabIndex: this.state.dropdownTrigger === itemKey ? 0 : -1,
 				toggleDropdown: this.toggleDropdown.bind(this, itemKey),
 			});
-		}
+		};
 
 		/**
 		 * Sets the active dropdown of the widget or discards the toggled item from the state.
@@ -67,7 +67,7 @@ export default WrappedComponent =>
 		 * @param {Object} itemDropdown The widget which requests to toggle its dropdown.
 		 * @param {Number} toggleDirection User movement direction when toggled via keyboard.
 		 */
-		toggleDropdown(itemDropdown, toggleDirection) {
+		toggleDropdown = (itemDropdown, toggleDirection) => {
 			this.setState(
 				{
 					dropdownTrigger: itemDropdown,
@@ -76,7 +76,7 @@ export default WrappedComponent =>
 							? itemDropdown
 							: null,
 				},
-				function() {
+				() => {
 					if (!this.state.itemDropdown) {
 						if (this.moveFocus) {
 							this.moveFocus(toggleDirection);
@@ -86,5 +86,5 @@ export default WrappedComponent =>
 					}
 				}
 			);
-		}
+		};
 	};

src/components/buttons/button-link-edit.jsx
@@ -148,10 +148,14 @@ class ButtonLinkEdit extends React.Component {
 			buttonClearLink = (
 				<button
 					aria-label={AlloyEditor.Strings.clearInput}
-					className="ae-button ae-icon-remove"
+					className="ae-button ae-button-clear"
 					onClick={this._clearLink}
-					title={AlloyEditor.Strings.clear}
-				/>
+					title={AlloyEditor.Strings.clear}>
+					<ButtonIcon
+						editor={this.props.editor}
+						symbol="times-circle"
+					/>
+				</button>
 			);
 		}
 

src/components/toolbars/toolbar-add.jsx
@@ -6,6 +6,7 @@ import WidgetDropdown from '../base/widget-dropdown.js';
 import WidgetExclusive from '../base/widget-exclusive.js';
 import WidgetFocusManager from '../base/widget-focus-manager.js';
 import WidgetPosition from '../base/widget-position.js';
+import ButtonIcon from '../buttons/button-icon.jsx';
 
 let POSITION_LEFT = 1;
 let POSITION_RIGHT = 2;

src/components/uibridge/button.jsx
@@ -40,9 +40,7 @@ import React from 'react';
 			ButtonBridge = class extends React.Component {
 				static displayName = buttonName;
 
-				statics = {
-					key: buttonName,
-				};
+				static key = buttonName;
 
 				// API not yet implemented inside the button
 				// bridge. By mocking the unsupported method, we prevent
@@ -99,7 +97,7 @@ import React from 'react';
 					);
 				}
 
-				_handleClick = event => {
+				_handleClick = () => {
 					var editor = this.props.editor.get('nativeEditor');
 
 					var buttonCommand =

src/components/uibridge/menu-button.jsx
@@ -45,9 +45,7 @@ import React from 'react';
 			MenuButtonBridge = class extends React.Component {
 				static displayName = menuButtonName;
 
-				statics = {
-					key: menuButtonName,
-				};
+				static key = menuButtonName;
 
 				// API not yet implemented inside the menubutton
 				// bridge. By mocking the unsupported method, we prevent

src/components/uibridge/panel-menu-button.jsx
@@ -37,9 +37,7 @@ import React from 'react';
 			PanelMenuButtonBridge = class extends React.Component {
 				static displayName = panelMenuButtonName;
 
-				statics = {
-					key: panelMenuButtonName,
-				};
+				static key = panelMenuButtonName;
 
 				// API not yet implemented inside the panel menu button bridge. By mocking the unsupported methods, we
 				// prevent plugins from crashing if they make use of them.

src/components/uibridge/richcombo.jsx
@@ -1,4 +1,5 @@
 import ButtonDropdown from '../buttons/button-dropdown.jsx';
+import ButtonIcon from '../buttons/button-icon.jsx';
 import React from 'react';
 
 (function() {
@@ -43,9 +44,7 @@ import React from 'react';
 			RichComboBridge = class extends React.Component {
 				static displayName = richComboName;
 
-				statics = {
-					key: richComboName,
-				};
+				static key = richComboName;
 
 				constructor(props) {
 					super(props);
@@ -128,7 +127,10 @@ import React from 'react';
 									<span className="ae-container-dropdown-selected-item">
 										{richComboLabel}
 									</span>
-									<span className="ae-icon-arrow" />
+									<ButtonIcon
+										editor={this.props.editor}
+										symbol="caret-bottom"
+									/>
 								</div>
 							</button>
 							{this.props.expanded && (

commit dd99f6d24dfaf70334dd48f99ca2666c5f96eeb2

Remove stray ">" from button-embed-edit.jsx, fixing 1 lint
Linter was complaining:

```
src/components/buttons/button-embed-edit.jsx
  147:6  error  HTML entities must be escaped  react/no-unescaped-entities
```

Looks like a bug introduced in 88ec95553. I love it when the linter
finds (non-stylistic) issues!

Related: https://github.com/liferay/alloy-editor/issues/990<Paste>

src/components/buttons/button-embed-edit.jsx
@@ -144,7 +144,6 @@ class ButtonEmbedEdit extends React.Component {
 						title={AlloyEditor.Strings.clear}>
 						<ButtonIcon editor={editor} symbol="times-clear" />
 					</button>
-					>
 				</div>
 				<button
 					aria-label={AlloyEditor.Strings.confirm}