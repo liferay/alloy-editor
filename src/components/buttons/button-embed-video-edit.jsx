liferay/alloy-editor

commit f9f3850163e6c1fa2aa21ebe9b8dece89087a626

Fix 9 remaining "guard-for-in" lints
```
src/plugins/dragresize_ie.js
  330:6   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
  554:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in

src/plugins/dragresize_ie11.js
   143:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
   384:6   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
  1038:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in

src/plugins/dragresize.js
  238:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
  327:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
  334:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
  340:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
```

One other one is fixed in: https://github.com/liferay/alloy-editor/pull/1018

This is pretty damn paranoid - some of these are operating on object
literals, so the only way the guard could prevent garbage from getting
through would be if somebody had defined enumerable properties on
`Object.prototype` - but it is better to be safe than sorry I guess. And
seeing as at least a couple of these are in modules built for IE, I'm
being super conservative and just applying the change suggested by the
linter.

Related: https://github.com/liferay/alloy-editor/issues/990

src/plugins/dragresize_ie11.js
@@ -140,7 +140,11 @@
 
 			let integrate = alignCommandIntegrator(editor);
 
-			for (let value in align) integrate(value);
+			for (let value in align) {
+				if (align.hasOwnProperty(value)) {
+					integrate(value);
+				}
+			}
 		},
 	});
 
@@ -381,8 +385,11 @@
 					!this.oldData.hasCaption &&
 					this.data.hasCaption
 				) {
-					for (let c in this.data.classes)
-						this.parts.image.removeClass(c);
+					for (let c in this.data.classes) {
+						if (this.data.classes.hasOwnProperty(c)) {
+							this.parts.image.removeClass(c);
+						}
+					}
 				}
 
 				// Set dimensions of the image according to gathered data.
@@ -1036,7 +1043,9 @@
 			// If there's an image, then cool, we got a widget.
 			// Now just remove dimension attributes expressed with %.
 			for (let d in dimensions) {
-				let dimension = image.attributes[d];
+				if (dimensions.hasOwnProperty(d)) {
+					let dimension = image.attributes[d];
+				}
 
 				if (dimension && dimension.match(regexPercent))
 					delete image.attributes[d];

src/plugins/dragresize.js
@@ -236,7 +236,9 @@
 			});
 
 			for (let n in handles) {
-				container.appendChild(handles[n]);
+				if (handles.hasOwnProperty(n)) {
+					container.appendChild(handles[n]);
+				}
 			}
 		},
 		createHandle: function(name) {
@@ -325,20 +327,31 @@
 			let handles = this.handles;
 
 			for (let handle in handles) {
-				POSITION_ELEMENT_FN[handle](handles[handle], left, top, box);
+				if (handles.hasOwnProperty(handle)) {
+					POSITION_ELEMENT_FN[handle](
+						handles[handle],
+						left,
+						top,
+						box
+					);
+				}
 			}
 		},
 		showHandles: function() {
 			let handles = this.handles;
 			this.updateHandles(this.box);
 			for (let n in handles) {
-				handles[n].style.display = 'block';
+				if (handles.hasOwnProperty(n)) {
+					handles[n].style.display = 'block';
+				}
 			}
 		},
 		hideHandles: function() {
 			let handles = this.handles;
 			for (let n in handles) {
-				handles[n].style.display = 'none';
+				if (handles.hasOwnProperty(n)) {
+					handles[n].style.display = 'none';
+				}
 			}
 		},
 		showPreview: function() {

src/plugins/dragresize_ie.js
@@ -328,7 +328,9 @@
 					this.data.hasCaption
 				) {
 					for (let c in this.data.classes) {
-						this.parts.image.removeClass(c);
+						if (this.data.classes.hasOwnProperty(c)) {
+							this.parts.image.removeClass(c);
+						}
 					}
 				}
 
@@ -552,10 +554,12 @@
 			// If there's an image, then cool, we got a widget.
 			// Now just remove dimension attributes expressed with %.
 			for (let d in dimensions) {
-				let dimension = image.attributes[d];
+				if (dimensions.hasOwnProperty(d)) {
+					let dimension = image.attributes[d];
 
-				if (dimension && dimension.match(regexPercent)) {
-					delete image.attributes[d];
+					if (dimension && dimension.match(regexPercent)) {
+						delete image.attributes[d];
+					}
 				}
 			}
 

commit 60bcc567ca3c2046ea68cdbabe3deb0949a6f6d6

Fix 8 more lints, including last of the "no-unused-vars"
Fixes:

```
src/components/buttons/button-embed-video-edit.jsx
  120:9   error  'icon' is assigned a value but never used              no-unused-vars

src/plugins/dragresize_ie11.js
    95:8   error  'config' is assigned a value but never used                                                                 no-unused-vars
  1045:4   error  The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype  guard-for-in
  1047:10  error  'dimension' is assigned a value but never used                                                              no-unused-vars
  1050:9   error  'dimension' is not defined                                                                                  no-undef
  1050:22  error  'dimension' is not defined                                                                                  no-undef

src/plugins/tableresize.js
  404:7   error  'destroy' is assigned a value but never used  no-unused-vars

src/plugins/tabletools.js
  803:8  error  'lang' is assigned a value but never used  no-unused-vars
```

The "dragresize_ie11.js" ones are a bit embarrassing, because I actually
caused 4 of those in (f9f3850163e6c1fa2) ie. #1019. This is why we need
QA I guess, because somehow our test suite isn't enough to catch
mistakes like this.

Test plan: `npm run dev && npm run test && npm run start` and check
demo.

Related: https://github.com/liferay/alloy-editor/issues/990

src/components/buttons/button-embed-video-edit.jsx
@@ -117,8 +117,6 @@ class ButtonEmbedVideoEdit extends React.Component {
 			opacity: this.state.videoURL ? 1 : 0,
 		};
 
-		const icon = this.getIcon();
-
 		return (
 			<div className="ae-container-embed-video-edit">
 				<div className="ae-container-input xxl">

src/plugins/dragresize_ie11.js
@@ -92,7 +92,6 @@
 		init: function(editor) {
 			// Adapts configuration from original image plugin. Should be removed
 			// when we'll rename ae_dragresize_ie11 to image.
-			let config = editor.config;
 
 			let image = widgetDef(editor);
 
@@ -1045,10 +1044,9 @@
 			for (let d in dimensions) {
 				if (dimensions.hasOwnProperty(d)) {
 					let dimension = image.attributes[d];
+					if (dimension && dimension.match(regexPercent))
+						delete image.attributes[d];
 				}
-
-				if (dimension && dimension.match(regexPercent))
-					delete image.attributes[d];
 			}
 
 			return el;

src/plugins/tableresize.js
@@ -401,13 +401,13 @@
 			resizer.setStyle('left', pxUnit(resizerNewPosition));
 		});
 
-		let destroy = (this.destroy = function() {
+		this.destroy = function() {
 			detach();
 
 			document.getBody().setStyle('cursor', 'auto');
 
 			resizer.remove();
-		});
+		};
 
 		var isResizing = (this.isResizing = function() {
 			return resizing;

src/plugins/tabletools.js
@@ -800,8 +800,6 @@
 
 	CKEDITOR.plugins.add('ae_tabletools', {
 		init: function(editor) {
-			let lang = editor.lang.table;
-
 			function createDef(def) {
 				return CKEDITOR.tools.extend(def || {}, {
 					contextSensitive: 1,

test/plugins/test/autolink.js
@@ -14,7 +14,6 @@
 	var KEY_SPACE = 32;
 
 	describe('AutoLink', function() {
-
 		before(function(done) {
 			Utils.createCKEditor.call(this, done, {
 				extraPlugins: 'ae_autolink',

test/ui/test/button-bridge.jsx
@@ -6,7 +6,6 @@
 	var Simulate = TestUtils.Simulate;
 
 	describe('ButtonBridge', function() {
-
 		before(function(done) {
 			Utils.createAlloyEditor.call(this, done, {
 				extraPlugins: