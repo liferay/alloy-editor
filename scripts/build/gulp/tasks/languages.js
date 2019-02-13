liferay/alloy-editor

commit 2b0f4c24058231c9e97bce4d3f09e2f9a591adce

Removes unnecessary hashing so buildLanguages always runs and attempts to prettify the result a little bit

scripts/build/gulp/tasks/languages.js
@@ -3,17 +3,10 @@
 const Constants = require('../constants');
 const fs = require('fs');
 const gulp = require('gulp');
-const hashFiles = require('hash-files');
 const path = require('path');
 const walk = require('walk');
 
-const hashSources = [
-	path.join(Constants.rootDir, 'lib', 'lang/*.js'),
-	Constants.ckeditorLangKeys,
-	path.join(Constants.langDir + '/*.json'),
-];
-
-const hashFile = path.join('./', '_hash');
+const langWhitelist = /^\w\.js/;
 
 /**
  * Normalizes the different string values that can be stored in a language template.
@@ -31,7 +24,7 @@ const getStringLangValue = function(value, lang) {
 	return eval(value);
 };
 
-function updateLangFiles(callback) {
+function buildLanguages(callback) {
 	// Mock the CKEDITOR.lang object to walk the ckeditor js lang files
 	global.CKEDITOR = {
 		lang: {},
@@ -52,6 +45,12 @@ function updateLangFiles(callback) {
 
 	// Iterate over every existing lang file inside src/lang
 	langWalker.on('file', (root, fileStats, next) => {
+		if (!langWhitelist.test(fileStats.name)) {
+			next();
+
+			return;
+		}
+
 		const lang = path.basename(fileStats.name, '.js');
 
 		// Load the matching CKEDITOR lang file with all the strings
@@ -89,7 +88,10 @@ function updateLangFiles(callback) {
 		fs.writeFile(
 			path.join(Constants.rootDir, 'src', 'lang', fileStats.name),
 			'AlloyEditor.Strings = ' +
-				JSON.stringify(AlloyEditor.Strings) +
+				JSON.stringify(AlloyEditor.Strings, null, 2).replace(
+					/^  /gm,
+					'\t'
+				) +
 				';',
 			err => {
 				if (err) {
@@ -98,54 +100,6 @@ function updateLangFiles(callback) {
 				next();
 			}
 		);
-	});
-}
-
-function createHash(callback) {
-	hashFiles({files: hashSources}, (err, hash) => {
-		if (err) {
-			return callback(err);
-		}
-
-		fs.writeFile(hashFile, hash, err => {
-			if (err) {
-				return callback(err);
-			}
-			callback();
-		});
-	});
-}
-
-function compareHash(originalHash, callback) {
-	hashFiles({files: hashSources}, (err, hash) => {
-		if (err) {
-			return callback(err);
-		}
-
-		const changed = originalHash !== hash;
-		callback(changed);
-	});
-}
-
-function buildLanguages(callback) {
-	fs.exists(hashFile, exists => {
-		if (!exists) {
-			updateLangFiles(() => createHash(callback));
-		} else {
-			fs.readFile(hashFile, (err, data) => {
-				if (err) {
-					return callback(err);
-				}
-
-				compareHash(data.toString(), changed => {
-					if (changed) {
-						updateLangFiles(() => createHash(callback));
-					} else {
-						callback();
-					}
-				});
-			});
-		}
 	});
 }
 

.prettierignore
@@ -1,0 +1,1 @@
+src/lang/*

scripts/build/gulp/_hash
@@ -1,1 +1,0 @@
-cc3dcb64e9a96a8c6ea572d15efbb68a3b70ca02

src/lang/.prettierignore
@@ -1,1 +1,0 @@
-*

commit 42cbfdaf928ed666e2ffd347c3608445dc085ffd

Fix broken language asset updates
Updates were broken in 2b0f4c240, which added a regex which couldn't
possibly match any of the files because none of them are of the form
"a.js". The new regex added in this commit matches "en.js", "en-au.js"
etc. We could make it stricter, but I don't think that's necessary.

scripts/build/gulp/tasks/languages.js
@@ -6,7 +6,7 @@ const gulp = require('gulp');
 const path = require('path');
 const walk = require('walk');
 
-const langWhitelist = /^\w\.js/;
+const langWhitelist = /^[\w-]+\.js/;
 
 /**
  * Normalizes the different string values that can be stored in a language template.