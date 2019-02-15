3addf06111aa48570e15c81802e33933c94e3fdd
Greg Hurrell <greg.hurrell@liferay.com>
2019-02-15 14:05:13 +0100

    Port <ButtonEmbed /> to use hooks
    Test plan: `npm run dev && npm run test && npm run start`; in the demo,
    click the left-hand "+" menu and see the button render; click the "+"
    icon, see the button render again in "exclusive" mode (ie.with text
    field etc); select some text, hit Command-L and see toolbar pop up with
    button in exclusive mode again. Edit link, see it work.

--- src/components/buttons/button-embed.jsx
+++ src/components/buttons/button-embed.jsx
@@ -1,82 +1,55 @@
+import React from 'react';
 import ButtonEmbedEdit from './button-embed-edit.jsx';
 import ButtonIcon from './button-icon.jsx';
-import ButtonKeystroke from '../base/button-keystroke';
-import React from 'react';
+import useButtonKeystroke from '../hooks/use-button-keystroke';
 
 /**
- * The ButtonEmbed class provides functionality for creating and editing an embed link in a document.
- * ButtonEmbed renders in two different modes:
+ * The ButtonEmbed component provides functionality for creating and editing
+ * an embed link in a document.  ButtonEmbed renders in two different
+ * modes:
  *
- * - Normal: Just a button that allows to switch to the edition mode
- * - Exclusive: The ButtonEmbedEdit UI with all the link edition controls.
- *
- * @class ButtonEmbed
- * @uses ButtonKeystroke
+ * - Normal: Just a button that allows to switch to the editing mode
+ * - Exclusive: The ButtonEmbedEdit UI with all the link editing controls.
  */
-class ButtonEmbed extends React.Component {
-	/**
-	 * Lifecycle. Returns the default values of the properties used in the widget.
-	 *
-	 * @instance
-	 * @memberof ButtonEmbed
-	 * @method getDefaultProps
-	 * @return {Object} The default properties.
-	 */
-	static defaultProps = {
-		keystroke: {
-			fn: '_requestExclusive',
-			keys: CKEDITOR.CTRL + CKEDITOR.SHIFT + 76 /* L*/,
-			name: 'embed',
-		},
-	};
+function ButtonEmbed({
+	keystroke = {
+		fn: () => {},
+		keys: CKEDITOR.CTRL + CKEDITOR.SHIFT + 76 /* L*/,
+		name: 'embed',
+	},
 
-	/**
-	 * The name which will be used as an alias of the button in the configuration.
-	 *
-	 * @default embed
-	 * @memberof ButtonEmbed
-	 * @property {String} key
-	 * @static
-	 */
-	static key = 'embed';
+	renderExclusive,
+	requestExclusive,
+	tabIndex,
 
-	/**
-	 * Lifecycle. Renders the UI of the button.
-	 *
-	 * @instance
-	 * @memberof ButtonEmbed
-	 * @method render
-	 * @return {Object} The content which should be rendered.
-	 */
-	render() {
-		if (this.props.renderExclusive) {
-			return <ButtonEmbedEdit {...this.props} />;
-		} else {
-			return (
-				<button
-					aria-label={AlloyEditor.Strings.link}
-					className="ae-button"
-					data-type="button-embed"
-					onClick={this._requestExclusive}
-					tabIndex={this.props.tabIndex}
-					title={AlloyEditor.Strings.link}>
-					<ButtonIcon symbol="plus" />
-				</button>
-			);
-		}
+	...restProps
+}) {
+	const execCommand = () => requestExclusive(ButtonEmbed.key);
+	useButtonKeystroke({
+		...keystroke,
+		fn: execCommand,
+	});
+
+	if (renderExclusive) {
+		return <ButtonEmbedEdit {...restProps} />;
+	} else {
+		return (
+			<button
+				aria-label={AlloyEditor.Strings.link}
+				className="ae-button"
+				data-type="button-embed"
+				onClick={execCommand}
+				tabIndex={tabIndex}
+				title={AlloyEditor.Strings.link}>
+				<ButtonIcon symbol="plus" />
+			</button>
+		);
 	}
-
-	/**
-	 * Requests the link button to be rendered in exclusive mode to allow the embedding of a link.
-	 *
-	 * @instance
-	 * @memberof ButtonEmbed
-	 * @method _requestExclusive
-	 * @protected
-	 */
-	_requestExclusive = () => {
-		this.props.requestExclusive(ButtonEmbed.key);
-	};
 }
 
-export default ButtonKeystroke(ButtonEmbed);
+/**
+ * The name which will be used as an alias of the button in the configuration.
+ */
+ButtonEmbed.key = 'embed';
+
+export default ButtonEmbed;
