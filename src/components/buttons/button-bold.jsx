70f34086cee0ac54a17ecd8b04bf11c5977a2671
Greg Hurrell <greg.hurrell@liferay.com>
2019-02-15 13:32:58 +0100

    Port <ButtonBold /> to use hooks
    Almost identical to the `<ButtonUnderline />` implementation, which
    suggests that we might want to factor out something common later. I'm
    not going to do that though until I've made an initial pass over all the
    buttons though, so that we can see all the patterns.

--- src/components/buttons/button-bold.jsx
+++ src/components/buttons/button-bold.jsx
@@ -1,74 +1,55 @@
-import ButtonCommand from '../base/button-command';
-import ButtonKeystroke from '../base/button-keystroke';
-import ButtonStateClasses from '../base/button-state-classes';
-import ButtonStyle from '../base/button-style';
-import ButtonIcon from './button-icon.jsx';
 import React from 'react';
+import ButtonIcon from './button-icon.jsx';
+import useExecCommand from '../hooks/use-exec-command';
+import useButtonStateClasses from '../hooks/use-button-state-classes';
+import useButtonStyle from '../hooks/use-button-style';
+import useButtonKeystroke from '../hooks/use-button-keystroke';
 
 /**
- * The ButtonBold class provides functionality for styling an selection with strong (bold) style.
- *
- * @class ButtonBold
- * @uses ButtonCommand
- * @uses ButtonKeystroke
- * @uses ButtonStateClasses
- * @uses ButtonStyle
+ * The ButtonBold component provides functionality for styling a selection
+ * with strong (bold) style.
  */
-class ButtonBold extends React.Component {
-	/**
-	 * Lifecycle. Returns the default values of the properties used in the widget.
-	 *
-	 * @instance
-	 * @memberof ButtonBold
-	 * @method getDefaultProps
-	 * @return {Object} The default properties.
-	 */
-	static defaultProps = {
-		command: 'bold',
-		keystroke: {
-			fn: 'execCommand',
-			keys: CKEDITOR.CTRL + 66 /* B*/,
-			name: 'bold',
-		},
-		style: 'coreStyles_bold',
-	};
+function ButtonBold({
+	command = 'bold',
+	keystroke = {
+		fn: () => {},
+		keys: CKEDITOR.CTRL + 66 /* B*/,
+		name: 'bold',
+	},
+	style = 'coreStyles_bold',
 
-	/**
-	 * The name which will be used as an alias of the button in the configuration.
-	 *
-	 * @default bold
-	 * @memberof ButtonBold
-	 * @property {String} key
-	 * @static
-	 */
-	static key = 'bold';
+	// TODO: kill this feature? nobody ever passes this prop
+	modifiesSelection,
 
-	/**
-	 * Lifecycle. Renders the UI of the button.
-	 *
-	 * @instance
-	 * @memberof ButtonBold
-	 * @method render
-	 * @return {Object} The content which should be rendered.
-	 */
-	render() {
-		const cssClass = `ae-button  ${this.getStateClasses()}`;
+	tabIndex,
+}) {
+	const execCommand = useExecCommand(command, modifiesSelection);
+	const stateClasses = useButtonStateClasses(style);
+	const [isActive, _style] = useButtonStyle(style);
+	const classNames = `ae-button ${stateClasses}`;
 
-		return (
-			<button
-				aria-label={AlloyEditor.Strings.bold}
-				aria-pressed={cssClass.indexOf('pressed') !== -1}
-				className={cssClass}
-				data-type="button-bold"
-				onClick={this.execCommand}
-				tabIndex={this.props.tabIndex}
-				title={AlloyEditor.Strings.bold}>
-				<ButtonIcon symbol="bold" />
-			</button>
-		);
-	}
+	useButtonKeystroke({
+		...keystroke,
+		fn: execCommand,
+	});
+
+	return (
+		<button
+			aria-label={AlloyEditor.Strings.bold}
+			aria-pressed={isActive}
+			className={classNames}
+			data-type="button-bold"
+			onClick={execCommand}
+			tabIndex={tabIndex}
+			title={AlloyEditor.Strings.bold}>
+			<ButtonIcon symbol="bold" />
+		</button>
+	);
 }
 
-export default ButtonCommand(
-	ButtonKeystroke(ButtonStateClasses(ButtonStyle(ButtonBold)))
-);
+/**
+ * The name which will be used as an alias of the button in the configuration.
+ */
+ButtonBold.key = 'bold';
+
+export default ButtonBold;
