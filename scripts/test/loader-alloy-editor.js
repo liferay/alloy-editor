4743478a353f9b6dadfcea7d8fea3e004d1c3d7f
Greg Hurrell <greg.hurrell@liferay.com>
2019-02-19 10:41:14 +0100

    Remove unnecessary IIFE wrapper from scripts/test/loader-alloy-editor.js
    There is no need to avoiding polluting the global scope here; the entire
    point of the loader *is* to pollute the global scope.

--- scripts/test/loader-alloy-editor.js
+++ scripts/test/loader-alloy-editor.js
@@ -5,25 +5,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 
-(function() {
-	if (!window.EditorContext) {
-		window.EditorContext = EditorContext;
-	}
+if (!window.EditorContext) {
+	window.EditorContext = EditorContext;
+}
 
-	if (!window.React) {
-		window.React = React;
-	}
+if (!window.React) {
+	window.React = React;
+}
 
-	if (!window.ReactDOM) {
-		window.ReactDOM = ReactDOM;
-	}
+if (!window.ReactDOM) {
+	window.ReactDOM = ReactDOM;
+}
 
-	if (!window.ReactTestUtils) {
-		window.ReactTestUtils = ReactTestUtils;
-	}
+if (!window.ReactTestUtils) {
+	window.ReactTestUtils = ReactTestUtils;
+}
 
-	if (!window.AlloyEditor) {
-		window.AlloyEditor = AlloyEditor;
-		window.ALLOYEDITOR_BASEPATH = 'dist/alloy-editor/';
-	}
-})();
+if (!window.AlloyEditor) {
+	window.AlloyEditor = AlloyEditor;
+	window.ALLOYEDITOR_BASEPATH = 'dist/alloy-editor/';
+}
