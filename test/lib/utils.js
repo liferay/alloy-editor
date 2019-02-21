aa53ac5d7237817e43ba7f48b07603be492f9b3f
Greg Hurrell <greg.hurrell@liferay.com>
2019-02-21 10:17:11 +0100

    utils.js: use ES6 object concise method syntax

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -1,250 +1,246 @@
 if (!window.Utils) {
-	window.Utils = {};
-}
+	const Utils = {
+		assertDropdownCommandButtonResult(config) {
+			var assertResult = window.Utils.assertResult(
+				'test/ui/test/fixtures'
+			);
 
-window.Utils.assertDropdownCommandButtonResult = function assertDropdownCommandButtonResult(
-	config
-) {
-	var assertResult = window.Utils.assertResult('test/ui/test/fixtures');
+			var TestUtils = ReactTestUtils;
+			var Simulate = TestUtils.Simulate;
 
-	var TestUtils = ReactTestUtils;
-	var Simulate = TestUtils.Simulate;
-
-	var command = function() {
-		var dropdown = TestUtils.findAllInRenderedTree(
-			config.buttonDropdown,
-			function(component) {
-				return TestUtils.isCompositeComponentWithType(
-					component,
-					config.buttonCommandsList
+			var command = function() {
+				var dropdown = TestUtils.findAllInRenderedTree(
+					config.buttonDropdown,
+					function(component) {
+						return TestUtils.isCompositeComponentWithType(
+							component,
+							config.buttonCommandsList
+						);
+					}
 				);
-			}
-		);
 
-		assert.ok(dropdown);
-		assert.equal(1, dropdown.length);
+				assert.ok(dropdown);
+				assert.equal(1, dropdown.length);
 
-		var commandButtons = TestUtils.findAllInRenderedTree(
-			dropdown[0],
-			function(component) {
-				return (
-					!TestUtils.isDOMComponent(component) &&
-					component.props.command === config.buttonCommand
+				var commandButtons = TestUtils.findAllInRenderedTree(
+					dropdown[0],
+					function(component) {
+						return (
+							!TestUtils.isDOMComponent(component) &&
+							component.props.command === config.buttonCommand
+						);
+					}
 				);
-			}
-		);
 
-		assert.ok(commandButtons.length);
+				assert.ok(commandButtons.length);
 
-		if (config.selectionFn) {
-			config.selectionFn.call(this);
-		}
+				if (config.selectionFn) {
+					config.selectionFn.call(this);
+				}
 
-		Simulate.click(ReactDOM.findDOMNode(commandButtons[0]));
-	};
+				Simulate.click(ReactDOM.findDOMNode(commandButtons[0]));
+			};
 
-	assertResult.call(
-		this,
-		config.initialFixture,
-		command,
-		config.expectedFixture,
-		config.errorMessage
-	);
-};
-
-window.Utils.createAlloyEditor = function createAlloyEditor(done, config) {
-	assert.ok(done);
-	assert.ok(bender);
-	assert.ok(CKEDITOR);
-	assert.ok(AlloyEditor);
-
-	window.Utils.createContainer.call(this);
-
-	var editable = document.createElement('div');
-
-	editable.setAttribute('id', 'editable');
-	editable.setAttribute('contenteditable', true);
-
-	document.getElementsByTagName('body')[0].appendChild(editable);
-
-	this._editable = editable;
-
-	config = CKEDITOR.tools.merge(
-		{
-			toolbars: {},
+			assertResult.call(
+				this,
+				config.initialFixture,
+				command,
+				config.expectedFixture,
+				config.errorMessage
+			);
 		},
-		config
-	);
 
-	this.editor = AlloyEditor.editable('editable', config);
+		createAlloyEditor(done, config) {
+			assert.ok(done);
+			assert.ok(bender);
+			assert.ok(CKEDITOR);
+			assert.ok(AlloyEditor);
 
-	this.nativeEditor = this.editor.get('nativeEditor');
+			window.Utils.createContainer.call(this);
 
-	this.nativeEditor.on(
-		'instanceReady',
-		function() {
-			window.Utils.focusEditor(this.nativeEditor);
-			done();
-		}.bind(this)
-	);
+			var editable = document.createElement('div');
 
-	this.render = function(component, container) {
-		return ReactDOM.render(
-			React.createElement(
-				EditorContext.Provider,
-				{value: {editor: this.editor}},
-				component
-			),
-			container
-		);
-	}.bind(this);
-};
+			editable.setAttribute('id', 'editable');
+			editable.setAttribute('contenteditable', true);
 
-window.Utils.destroyAlloyEditor = function destroyAlloyEditor(done) {
-	if (this.editor) {
-		this.editor.destroy();
-		this.editor = null;
-	}
+			document.getElementsByTagName('body')[0].appendChild(editable);
 
-	this._editable.parentNode.removeChild(this._editable);
-	this._editable = null;
-	Utils.removeContainer.call(this);
-	fixture.cleanup();
+			this._editable = editable;
 
-	if (done) {
-		done();
-	}
-};
+			config = CKEDITOR.tools.merge(
+				{
+					toolbars: {},
+				},
+				config
+			);
 
+			this.editor = AlloyEditor.editable('editable', config);
 
-if (!window.Utils) {
-	window.Utils = {};
-}
+			this.nativeEditor = this.editor.get('nativeEditor');
 
-window.Utils.assertResult = function assertResult(fixtureBase) {
-	var getFixture = Utils.getFixture(fixtureBase);
+			this.nativeEditor.on(
+				'instanceReady',
+				function() {
+					window.Utils.focusEditor(this.nativeEditor);
+					done();
+				}.bind(this)
+			);
 
-	return function(initialFixture, command, expectedFixture, message) {
-		var initial = getFixture(initialFixture);
-		var expected = getFixture(expectedFixture);
+			this.render = function(component, container) {
+				return ReactDOM.render(
+					React.createElement(
+						EditorContext.Provider,
+						{value: {editor: this.editor}},
+						component
+					),
+					container
+				);
+			}.bind(this);
+		},
 
-		bender.tools.selection.setWithHtml(this.nativeEditor, initial);
-
-		command.call(this);
-
-		var data = bender.tools.getData(this.nativeEditor, {
-			fixHtml: true,
-			compatHtml: true,
-		});
-
-		assert.strictEqual(data, expected, message);
-	};
-};
-
-window.Utils.createCKEditor = function createCKEditor(
-	done,
-	config,
-	attributes
-) {
-	assert.ok(done);
-	assert.ok(bender);
-	assert.ok(CKEDITOR);
-
-	Utils.createContainer.call(this);
-
-	var editable = document.createElement('div');
-
-	editable.setAttribute('id', 'editable');
-	editable.setAttribute('contenteditable', true);
-
-	if (attributes) {
-		for (var attribute in attributes) {
-			if (Object.prototype.hasOwnProperty.call(attributes, attribute)) {
-				editable.setAttribute(attribute, attributes[attribute]);
+		destroyAlloyEditor(done) {
+			if (this.editor) {
+				this.editor.destroy();
+				this.editor = null;
 			}
-		}
-	}
 
-	const cruft = document.querySelectorAll('#editable');
-	if (cruft.length) {
-		console.warn('Pre-existing #editable element found in DOM!');
-		for (var i = 0; i < cruft.length; i++) {
-			cruft[i].parentNode.removeChild(cruft[i]);
-		}
-	}
+			this._editable.parentNode.removeChild(this._editable);
+			this._editable = null;
+			Utils.removeContainer.call(this);
+			fixture.cleanup();
 
-	var body = document.getElementsByTagName('body')[0];
-	body.appendChild(editable);
+			if (done) {
+				done();
+			}
+		},
 
-	this._editable = editable;
+		assertResult(fixtureBase) {
+			var getFixture = Utils.getFixture(fixtureBase);
 
-	this.nativeEditor = CKEDITOR.inline('editable', config);
+			return function(initialFixture, command, expectedFixture, message) {
+				var initial = getFixture(initialFixture);
+				var expected = getFixture(expectedFixture);
 
-	this.nativeEditor.on(
-		'instanceReady',
-		function() {
-			this.nativeEditor.focus();
-			done();
-		}.bind(this)
-	);
-};
+				bender.tools.selection.setWithHtml(this.nativeEditor, initial);
 
-window.Utils.createContainer = function createContainer() {
-	this.container = document.createElement('div');
+				command.call(this);
 
-	document.body.appendChild(this.container);
-};
+				var data = bender.tools.getData(this.nativeEditor, {
+					fixHtml: true,
+					compatHtml: true,
+				});
 
-window.Utils.destroyCKEditor = function destroyCKEditor(done) {
-	if (this.nativeEditor) {
-		this.nativeEditor.destroy();
-		this.nativeEditor = null;
-	}
+				assert.strictEqual(data, expected, message);
+			};
+		},
 
-	this._editable.parentNode.removeChild(this._editable);
-	this._editable = null;
-	Utils.removeContainer.call(this);
-	fixture.cleanup();
+		createCKEditor(done, config, attributes) {
+			assert.ok(done);
+			assert.ok(bender);
+			assert.ok(CKEDITOR);
 
-	if (done) {
-		done();
-	}
-};
+			Utils.createContainer.call(this);
 
-window.Utils.focusEditor = function focusEditor(editor) {
-	editor.focus();
+			var editable = document.createElement('div');
 
-	// Firefox needs a stronger focus simulation
-	editor.editable().fire('focus', {$: {}});
-};
+			editable.setAttribute('id', 'editable');
+			editable.setAttribute('contenteditable', true);
 
-window.Utils.getFixture = function getFixture(fixtureBase) {
-	return function(fixtureFile) {
-		fixture.setBase(fixtureBase);
+			if (attributes) {
+				for (var attribute in attributes) {
+					if (
+						Object.prototype.hasOwnProperty.call(
+							attributes,
+							attribute
+						)
+					) {
+						editable.setAttribute(attribute, attributes[attribute]);
+					}
+				}
+			}
 
-		fixture.load(fixtureFile);
+			const cruft = document.querySelectorAll('#editable');
+			if (cruft.length) {
+				console.warn('Pre-existing #editable element found in DOM!');
+				for (var i = 0; i < cruft.length; i++) {
+					cruft[i].parentNode.removeChild(cruft[i]);
+				}
+			}
 
-		return Utils._prepareFixtureForAssertion(fixture.el);
+			var body = document.getElementsByTagName('body')[0];
+			body.appendChild(editable);
+
+			this._editable = editable;
+
+			this.nativeEditor = CKEDITOR.inline('editable', config);
+
+			this.nativeEditor.on(
+				'instanceReady',
+				function() {
+					this.nativeEditor.focus();
+					done();
+				}.bind(this)
+			);
+		},
+
+		createContainer() {
+			this.container = document.createElement('div');
+
+			document.body.appendChild(this.container);
+		},
+
+		destroyCKEditor(done) {
+			if (this.nativeEditor) {
+				this.nativeEditor.destroy();
+				this.nativeEditor = null;
+			}
+
+			this._editable.parentNode.removeChild(this._editable);
+			this._editable = null;
+			Utils.removeContainer.call(this);
+			fixture.cleanup();
+
+			if (done) {
+				done();
+			}
+		},
+
+		focusEditor(editor) {
+			editor.focus();
+
+			// Firefox needs a stronger focus simulation
+			editor.editable().fire('focus', {$: {}});
+		},
+
+		getFixture(fixtureBase) {
+			return function(fixtureFile) {
+				fixture.setBase(fixtureBase);
+
+				fixture.load(fixtureFile);
+
+				return Utils._prepareFixtureForAssertion(fixture.el);
+			};
+		},
+
+		removeContainer() {
+			this.container.parentNode.removeChild(this.container);
+			this.container = null;
+		},
+
+		_prepareFixtureForAssertion(htmlFixture) {
+			var fixtureString;
+
+			if (htmlFixture) {
+				fixtureString = bender.tools.fixHtml(
+					bender.tools.compatHtml(
+						htmlFixture.innerHTML.replace(/\u00a0/g, '&nbsp;')
+					)
+				);
+			}
+
+			return fixtureString;
+		},
 	};
-};
 
-window.Utils.removeContainer = function removeContainer() {
-	this.container.parentNode.removeChild(this.container);
-	this.container = null;
-};
-
-window.Utils._prepareFixtureForAssertion = function _prepareFixtureForAssertion(
-	htmlFixture
-) {
-	var fixtureString;
-
-	if (htmlFixture) {
-		fixtureString = bender.tools.fixHtml(
-			bender.tools.compatHtml(
-				htmlFixture.innerHTML.replace(/\u00a0/g, '&nbsp;')
-			)
-		);
-	}
-
-	return fixtureString;
-};
+	window.Utils = Utils;
+}
