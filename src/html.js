6471afede3e5c8d13573e3eb63a1a4020853efbf
Greg Hurrell <greg.hurrell@liferay.com>
2019-04-01 14:13:06 +0200

    fix: run Prettier

--- src/html.js
+++ src/html.js
@@ -11,14 +11,35 @@ class HTML extends Component {
 						name="viewport"
 						content="width=device-width, initial-scale=1.0"
 					/>
-					<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-					<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-					<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/favicons/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicons/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicons/favicon-16x16.png"
+					/>
 					<link rel="manifest" href="/favicons/site.webmanifest" />
-					<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+					<link
+						rel="mask-icon"
+						href="/favicons/safari-pinned-tab.svg"
+						color="#5bbad5"
+					/>
 					<link rel="shortcut icon" href="/favicons/favicon.ico" />
 					<meta name="msapplication-TileColor" content="#ffffff" />
-					<meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+					<meta
+						name="msapplication-config"
+						content="/favicons/browserconfig.xml"
+					/>
 					<meta name="theme-color" content="#ffffff" />
 					{this.props.headComponents}
 					<link
