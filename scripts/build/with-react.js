b1c5ccc660f41ff3c57f08f4e6dfd0657e6ba8a6
Greg Hurrell <greg@hurrell.net>
2019-04-01 10:08:28 +0200

    fix: missing PropTypes on AlloyEditor.React (#1200)
    We still use PropTypes internally, even though it is no longer
    bundled with React. Just had a report from @ricardocousolr about
    a failure on current master stemming from trying to access
    `React.AlloyEditor.PropTypes`. Given that we still use it, we should
    expose it in the same way as before as people may be relying on it.
    
    Test plan: swap out the build in the demo for the `no-ckeditor` one
    (which exposes React, and which we use in DXP), and in browser console
    verify that I can access `AlloyEditor.React.PropTypes`.
    
    ```
    diff --git a/scripts/build/gulp/demo/index.html b/scripts/build/gulp/demo/index.html
    index 93f8964c..62fb48e3 100644
    --- a/scripts/build/gulp/demo/index.html
    +++ b/scripts/build/gulp/demo/index.html
    @@ -10,7 +10,8 @@
    
             <link href="alloy-editor/assets/alloy-editor-ocean.css" rel="stylesheet">
    
    -        <script src="alloy-editor/alloy-editor-all.js"></script>
    +        <script src="https://cdn.ckeditor.com/4.11.3/standard/ckeditor.js"></script>
    +        <script src="alloy-editor/alloy-editor-no-ckeditor.js"></script>
    
             <style>
                 body {
    ```
    
    Closes: https://github.com/liferay/alloy-editor/issues/1200

--- scripts/build/with-react.js
+++ scripts/build/with-react.js
@@ -1,10 +1,12 @@
 import * as AlloyEditor from '../../src/adapter/main';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 if (typeof window !== 'undefined') {
 	window.CKEDITOR.disableAutoInline = true;
 	window.AlloyEditor = AlloyEditor;
 	AlloyEditor.React = React;
+	AlloyEditor.React.PropTypes = PropTypes;
 }
 
 export {AlloyEditor};
