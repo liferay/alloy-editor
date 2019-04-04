342fd650fc1b246b3932e92b23659163266686f9
Greg Hurrell <greg.hurrell@liferay.com>
2019-04-04 13:20:51 +0200

    chore: fix formatting of scripts/build/version.js

--- scripts/build/version.js
+++ scripts/build/version.js
@@ -3,7 +3,6 @@ const version = require('../../package.json').version;
 
 module.exports = () => {
 	return {
-		code: `module.exports = ${JSON.stringify(version)};`
+		code: `module.exports = ${JSON.stringify(version)};`,
 	};
 };
-
