e492cd3a8770d74eef1f856e4723ce0521f99441
Greg Hurrell <greg.hurrell@liferay.com>
2019-09-25 16:39:17 +0200

    refactor: remove unreferenced "disabled" functionality (#1314)
    No references in this repo or in liferay-portal.

--- src/components/base/button-state-classes.js
+++ src/components/base/button-state-classes.js
@@ -6,7 +6,6 @@ import Lang from '../../oop/lang';
  *
  * To check for state, the component can expose the following methods:
  * - `Function` **isActive** to check the active state
- * - `Function` **isDisabled** to check the disabled state
  *
  * @class ButtonStateClasses
  */
@@ -14,7 +13,7 @@ export default WrappedComponent =>
 	class ButtonStateClasses extends WrappedComponent {
 		/**
 		 * Returns the list of state classes associated to the current element's state, according
-		 * to the results of the isActive and isDisabled methods.
+		 * to the results of the isActive method.
 		 *
 		 * @instance
 		 * @memberof ButtonStateClasses
@@ -27,11 +26,6 @@ export default WrappedComponent =>
 			// Check for active state
 			if (Lang.isFunction(this.isActive) && this.isActive()) {
 				stateClasses += 'ae-button-pressed';
-			}
-
-			// Check for disabled state
-			if (Lang.isFunction(this.isDisabled) && this.isDisabled()) {
-				stateClasses += ' ae-button-disabled';
 			}
 
 			return stateClasses;
