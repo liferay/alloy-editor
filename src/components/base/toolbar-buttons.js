bc7061825c000becc7ddd32236a890e304fdb12a
Greg Hurrell <greg.hurrell@liferay.com>
2019-09-27 13:51:58 +0200

    chore: run `yarn lint:fix` (#1314)

--- src/components/base/toolbar-buttons.js
+++ src/components/base/toolbar-buttons.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
 
 import EditorContext from '../../adapter/editor-context';
 import Lang from '../../oop/lang';
