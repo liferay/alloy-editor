89adc4fbbcfd69fe7db9080be654f622ca55c5c9
Dariusz Szut <dew326@gmail.com>
2020-07-22 11:35:32 +0200

    chore: Updated CKEditor build to v4.14.1

--- test/core/karma.js
+++ test/core/karma.js
@@ -42,6 +42,10 @@ var filesToLoad = [
         pattern: path.join(alloyEditorDir, 'lang/*.js'),
         included: true,
         watched: false
+    }, {
+        pattern: path.join(alloyEditorDir, 'vendor/promise.js'),
+        included: true,
+        watched: false
     },
 
     /* bender requires CKEDITOR, should be after ckeditor.js */
